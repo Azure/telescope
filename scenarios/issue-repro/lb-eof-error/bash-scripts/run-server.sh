0ddde3c8a8fd05d89e10fe3c07683db6035754b3
Anson Qian <ansonqian@microsoft.com>
2023-11-16 03:29:08 +0000

    add userdata bash scripts for lb eof error repro

--- scenarios/issue-repro/lb-eof-error/bash-scripts/run-server.sh
+++ scenarios/issue-repro/lb-eof-error/bash-scripts/run-server.sh
@@ -2,14 +2,8 @@
 
 tag=$1
 timeout=${2:-32}
-side="server"
 
-cd /home/adminuser
-curl -L -s -o ${side}.tar.gz https://github.com/anson627/cloud-lb-evaluator/releases/download/$tag/${side}.tar.gz
-tar -xzvf ${side}.tar.gz 
-rm ${side}.tar.gz
-
-cd ${side}_build
+cd server_build
 echo "Running server"
-./${side} $timeout &> logs.txt &
+./server $timeout &> logs.txt &
 ps -ef | grep $side
