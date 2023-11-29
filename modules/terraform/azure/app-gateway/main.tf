b5e3231c322d9e8b54e475d8948b98b18de9c088
Cesar Lazo <cesarlazo@microsoft.com>
2023-11-29 16:29:40 -0500

    adding more cahnges

--- modules/terraform/azure/app-gateway/main.tf
+++ modules/terraform/azure/app-gateway/main.tf
@@ -18,30 +18,22 @@ resource "azurerm_application_gateway" "appgateway" {
     name      = "my-gateway-ip-configuration"
     subnet_id = var.subnet_id
   }
- backend_address_pool {
-    name         = "aks-direct"
-    ip_addresses = ["10.10.1.7", "10.10.1.8", "10.10.1.9"]
-  }
-
-  frontend_ip_configuration {
-    name                 = "public"
-    public_ip_address_id = var.public_ip_id
-  }
-
-   http_listener {
-    name                           = "https-backend-contoso-com-lb"
-    frontend_ip_configuration_name = "public"
-    frontend_port_name             = "http"
-    protocol                       = "Http"
-    host_name                      = "https-backend-lb.contoso.com"
-  }
 
   frontend_port {
     name = "http"
     port = 80
   }
+ frontend_ip_configuration {
+    name                 = "public"
+    public_ip_address_id = var.public_ip_id
+  }
 
-backend_http_settings {
+ backend_address_pool {
+    name         = "aks-direct"
+    ip_addresses = ["10.10.1.7", "10.10.1.8", "10.10.1.9"]
+  }
+
+ backend_http_settings {
     name                           = "aks-https-lb"
     host_name                      = "test.contoso.com"
     cookie_based_affinity          = "Disabled"
@@ -51,8 +43,7 @@ backend_http_settings {
     trusted_root_certificate_names = ["self-signed-root"]
     probe_name                     = "aks-https"
   }
-
-  probe {
+probe {
     name                                      = "aks-https"
     protocol                                  = "Https"
     path                                      = "/"
@@ -60,6 +51,14 @@ backend_http_settings {
     interval                                  = 10
     timeout                                   = 30
     unhealthy_threshold                       = 3
+  }
+
+   http_listener {
+    name                           = "https-backend-contoso-com-lb"
+    frontend_ip_configuration_name = "public"
+    frontend_port_name             = "http"
+    protocol                       = "Http"
+    host_name                      = "https-backend-lb.contoso.com"
   }
 
   request_routing_rule {
@@ -71,8 +70,9 @@ backend_http_settings {
     backend_http_settings_name = "aks-https-lb"
   }
 
-  trusted_root_certificate {
+trusted_root_certificate {
     name = "self-signed-root"
     data = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tDQpNSUlCdHpDQ0FWMENGSEUvNk5mME92L3QxV2JCQlBTOWp2VlBJV0pOTUFvR0NDcUdTTTQ5QkFNQ01GNHhDekFKDQpCZ05WQkFZVEFrNU1NUTR3REFZRFZRUUlEQVZCZW5WeVpURVNNQkFHQTFVRUJ3d0pRVzF6ZEdWeVpHRnRNUkV3DQpEd1lEVlFRS0RBaHViR2xuYUhSbGJqRVlNQllHQTFVRUF3d1BjMlZzWm5OcFoyNWxaQzF5YjI5ME1CNFhEVEl6DQpNRFl5TWpFM05UQXpNMW9YRFRJME1EWXlNVEUzTlRBek0xb3dYakVMTUFrR0ExVUVCaE1DVGt3eERqQU1CZ05WDQpCQWdNQlVGNmRYSmxNUkl3RUFZRFZRUUhEQWxCYlhOMFpYSmtZVzB4RVRBUEJnTlZCQW9NQ0c1c2FXZG9kR1Z1DQpNUmd3RmdZRFZRUUREQTl6Wld4bWMybG5ibVZrTFhKdmIzUXdXVEFUQmdjcWhrak9QUUlCQmdncWhrak9QUU1CDQpCd05DQUFUTzZvVVpsRjBwRWdEME5nQ1Bsc1ptUjk2OVMrcHBzRlF1bVZFK1NYK1JkVDMwZ1BVRjFyRTB1WjZ2DQpLMWJRREhSSVV3bzNnZzJZTnZKb3BvbFVmL3VLTUFvR0NDcUdTTTQ5QkFNQ0EwZ0FNRVVDSVFDbDdlN1o0bHplDQoxTGowMS9zU1I2K0lCZHVESUpNTkQxamdsTTYvdDc0NXh3SWdZSHl3SjArNmw2SHgvT2tOTnlYZmxNalBvaWk0DQpoNHczNzQxNFZqMG56Qk09DQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tDQo="
   }
+
 }
