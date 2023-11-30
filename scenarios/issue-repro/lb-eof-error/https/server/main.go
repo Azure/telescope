99f0b7628e434175d64434e041ff7f2aedb3eede
Anson Qian <ansonqian@microsoft.com>
2023-11-30 23:31:42 +0000

    containerize lb issue repro code

--- scenarios/issue-repro/lb-eof-error/https/server/main.go
+++ scenarios/issue-repro/lb-eof-error/https/server/main.go
@@ -43,12 +43,12 @@ func getLocalIP() string {
 }
 
 func main() {
-	var timeout uint64
-	timeout = 32
+	var readHeaderTimeout uint64 = 32
+	readHeaderTimeout = 32
 	if len(os.Args) > 1 {
-		timeout, _ = strconv.ParseUint(os.Args[1], 10, 64)
+		readHeaderTimeout, _ = strconv.ParseUint(os.Args[1], 10, 64)
 	}
-	fmt.Printf("Set read header timeout to %v seconds\n", timeout)
+	fmt.Printf("Set read header timeout to %v seconds\n", readHeaderTimeout)
 
 	// Serve the healthz endpoint.
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -102,7 +102,7 @@ func main() {
 	httpsServer := &http.Server{
 		Addr:              ":4443",
 		TLSConfig:         tlsConfig,
-		ReadHeaderTimeout: time.Duration(int(timeout)) * time.Second, // https://github.com/kubernetes/kubernetes/blob/aa8cb97f65fe1d24e96eda129337d86109615570/staging/src/k8s.io/apiserver/pkg/server/secure_serving.go#L172
+		ReadHeaderTimeout: time.Duration(int(readHeaderTimeout)) * time.Second, // https://github.com/kubernetes/kubernetes/blob/aa8cb97f65fe1d24e96eda129337d86109615570/staging/src/k8s.io/apiserver/pkg/server/secure_serving.go#L172
 	}
 	tlsErrorWriter := &tlsHandshakeErrorWriter{os.Stderr}
 	tlsErrorLogger := log.New(tlsErrorWriter, "", 0)
