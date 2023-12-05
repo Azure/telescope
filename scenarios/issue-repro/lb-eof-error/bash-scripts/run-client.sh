c91236d1f362c56cb9086188c520ebd3a15dd442
alyssavu <alyssavu@microsoft.com>
2023-12-05 21:26:58 +0000

    update run-client script

--- scenarios/issue-repro/lb-eof-error/bash-scripts/run-client.sh
+++ scenarios/issue-repro/lb-eof-error/bash-scripts/run-client.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
-
-public_ip=${1:-''}
-
 docker run -d --name client -e SERVER_ADDRESS=$public_ip telescope.azurecr.io/issue-repro/slb-eof-error-client:v1.0.5
-docker ps --filter name=client
+
+code=$(docker wait client)
+if [[ $code -eq 0 ]]; then
+  tls_count=$(docker logs client | grep "TLS handshake timeout" | wc -l)
+  echo "TLS handshake timeout count: $tls_count"
+else
+  echo "Client exited with error"
+fi
+exit $code
