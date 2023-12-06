b190f77b08c7ba3d53e2f75e011c07a1a36c5c41
Alyssa Vu <49544763+alyssa1303@users.noreply.github.com>
2023-12-06 10:47:05 -0500

    Fix typo in run-client script for tls issue (#17)

--- scenarios/issue-repro/lb-tls-error/bash-scripts/run-client.sh
+++ scenarios/issue-repro/lb-tls-error/bash-scripts/run-client.sh
@@ -1,4 +1,4 @@
-docker run -d --name client -e SERVER_ADDRESS=$public_ip telescope.azurecr.io/issue-repro/slb-eof-error-client:v1.0.5 &> /dev/null
+docker run -d --name client -e SERVER_ADDRESS=$public_ip telescope.azurecr.io/issue-repro/slb-eof-error-client:v1.0.5 > /dev/null
 
 code=$(docker wait client)
 if [[ $code -eq 0 ]]; then
