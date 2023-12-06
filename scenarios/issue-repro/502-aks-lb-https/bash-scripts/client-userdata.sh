1f346c7c70ece8274a0f16d344ed6bbd1f05625c
Cesar Lazo <cesarlazo@microsoft.com>
2023-12-05 21:18:00 -0500

    installing kubectl

--- scenarios/issue-repro/502-aks-lb-https/bash-scripts/client-userdata.sh
+++ scenarios/issue-repro/502-aks-lb-https/bash-scripts/client-userdata.sh
@@ -1,6 +1,8 @@
 #dowloading kubctl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 
 #installing helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
