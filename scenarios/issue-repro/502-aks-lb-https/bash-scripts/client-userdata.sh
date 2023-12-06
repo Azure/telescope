0a943c1a58ee58ce2b810689144c5b38e511d5a0
Ubuntu <azureuser@linuxvm.of4bge4kse0e1loz2en2uj1cud.bx.internal.cloudapp.net>
2023-12-06 01:53:19 +0000

    adding initial file

---
+++ scenarios/issue-repro/502-aks-lb-https/bash-scripts/client-userdata.sh
@@ -0,0 +1,10 @@
+#dowloading kubctl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+#installing helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+
