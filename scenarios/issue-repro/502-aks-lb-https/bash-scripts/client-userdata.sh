6e5f9ae2e003e104ac7db72388319ccfeacd4f3f
Cesar Lazo <cesarlazo@microsoft.com>
2023-12-06 21:55:39 -0500

    adding latest changes

--- scenarios/issue-repro/502-aks-lb-https/bash-scripts/client-userdata.sh
+++ scenarios/issue-repro/502-aks-lb-https/bash-scripts/client-userdata.sh
@@ -11,3 +11,14 @@ sudo mv apache-jmeter-${jmeter_version} /opt/jmeter
 sudo ln -s /opt/jmeter/bin/jmeter /usr/local/bin/jmeter
 jmeter -v
 
+# install plugin
+cd /opt/jmeter/lib
+cmdrunner_version="2.2"
+curl -O "https://repo1.maven.org/maven2/kg/apc/cmdrunner/${cmdrunner_version}/cmdrunner-${cmdrunner_version}.jar"
+cd /opt/jmeter/lib/ext
+jpm_version="1.7"
+curl -O "https://repo1.maven.org/maven2/kg/apc/jmeter-plugins-manager/${jpm_version}/jmeter-plugins-manager-${jpm_version}.jar"
+cd /opt/jmeter/lib
+plugin_version="2.2"
+java -jar cmdrunner-${cmdrunner_version}.jar --tool org.jmeterplugins.repository.PluginManagerCMD install jpgc-synthesis,jpgc-cmd=${plugin_version}
+
