a51c5e3a87bd0726eecde3efbaaf00abdd739eb6
alyssavu <alyssavu@microsoft.com>
2023-12-07 20:48:15 +0000

    update script to output json

--- scenarios/issue-repro/lb-tls-error/bash-scripts/run-client.sh
+++ scenarios/issue-repro/lb-tls-error/bash-scripts/run-client.sh
@@ -3,8 +3,14 @@ docker run -d --name client -e SERVER_ADDRESS=$public_ip telescope.azurecr.io/is
 code=$(docker wait client)
 if [[ $code -eq 0 ]]; then
   tls_count=$(docker logs client | grep "TLS handshake timeout" | wc -l)
-  echo "TLS handshake timeout count: $tls_count"
+  result=$(jq -n \
+    --arg metric "TLS Handshake Timeout" \
+    --arg value "$tls_count" \
+    --arg unit "times" \
+    '{metric: $metric, value: $value, unit: $unit}')
+  echo $result
 else
   echo "Client exited with error"
 fi
+docker rm client > /dev/null
 exit $code
