9c2e5b272e482b1cf38644abdbfda9d02b2385b8
Sumanth Reddy Chinna Pullaiah (from Dev Box) <schinnapulla@microsoft.com>
2023-12-11 10:45:05 -0500

    fix return

--- modules/bash/aws.sh
+++ modules/bash/aws.sh
@@ -55,7 +55,6 @@ aws_check_ec2() {
 
       if [ "$instance_status" = "ok" ]; then
         echo "EC2 instance $instance_id is healthy."
-        echo $instance_id
         break
       elif [ "$i" -eq "$max_retries" ]; then
         echo "EC2 instance $instance_id not healthy after $max_retries retries"
@@ -66,6 +65,8 @@ aws_check_ec2() {
       fi
       i=$((i+1))
   done
+
+  echo $instance_id
 }
 
 aws_instance_ip_address() {
