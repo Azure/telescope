4733d9498180875be0c0c23bd8872f61ca5c4c42
alyssavu <alyssavu@microsoft.com>
2024-01-22 16:41:11 +0000

    update run_jmeter

--- modules/bash/jmeter.sh
+++ modules/bash/jmeter.sh
@@ -2,53 +2,29 @@
 
 source ./modules/bash/utils.sh
 
-check_jmeter_setup() {
-  local egress_ip_address=$1
-  local privatekey_path=$2
-
-  echo "Check jmeter setup"
-  commands=("java -version" "jmeter -v" "ls /opt/jmeter/lib/cmdrunner-2.2.jar" "ls /opt/jmeter/lib/ext/jmeter-plugins-manager-1.7.jar" "ls /opt/jmeter/bin/JMeterPluginsCMD.sh" "ls /opt/jmeter/lib/ext/jmeter-plugins-synthesis-2.2.jar")
-  for command in "${commands[@]}"
-  do
-    echo "run_ssh $privatekey_path ubuntu $egress_ip_address $command"
-    run_ssh $privatekey_path ubuntu $egress_ip_address 2222 "$command"
-    if [ "$?" -ne 0 ]; then
-      echo "Command $command failed with exit code $?"
-      exit 1
-    fi
-  done
-}
-
 run_jmeter() {
   local ingress_ip_address=$1
   local egress_ip_address=$2
-  local scenario_type=$3
-  local scenario_name=$4
-  local privatekey_path=$5
-  local protocol=$6
-  local port=$7
-  local concurrency=$8
-  local loop=$9
+  local privatekey_path=$3
+  local jmeter_file_dest=$4
+  local jmeter_result_file=$5
+  local jmeter_aggregate_file=$6
+  local jmeter_log_file=$7
+  local jmeter_properties=$8
+  local result_dir=$9
 
-  local jmeter_file_source=./scenarios/${scenario_type}/${scenario_name}/bash-scripts
-  local jmeter_file_dest=/tmp/jmeter
-
-  echo "Copy properties and jmx files"
-  run_scp_remote $privatekey_path ubuntu $egress_ip_address "${jmeter_file_source}/jmeter.properties" "${jmeter_file_dest}/jmeter.properties"
-  run_scp_remote $privatekey_path ubuntu $egress_ip_address "${jmeter_file_source}/https_test.jmx" "${jmeter_file_dest}/https_test.jmx"
-
-  sleep 5m
-  jmeterCommand="jmeter -n -t ${jmeter_file_dest}/https_test.jmx -f -p ${jmeter_file_dest}/jmeter.properties -Jprotocol=${protocol} -Jport=${port} -Jip_address=${ingress_ip_address} -Jthread_num=${concurrency} -Jloop_count=${loop} -Jresult_file_name=${jmeter_file_dest}/result-${protocol}-${concurrency} -j ${jmeter_file_dest}/jmeter-${protocol}-${concurrency}.log"
+  # sleep 5m
+  jmeterCommand="jmeter -n -t ${jmeter_file_dest}/https_test.jmx -f -p ${jmeter_file_dest}/jmeter.properties -j ${jmeter_file_dest}/${jmeter_log_file}.log ${jmeter_properties}"
   echo "Run test command: $jmeterCommand"
   run_ssh $privatekey_path ubuntu $egress_ip_address 2222 "$jmeterCommand"
 
-  aggregateCommand="java -jar /opt/jmeter/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv ${jmeter_file_dest}/aggregate-${protocol}-${concurrency}.csv --input-jtl ${jmeter_file_dest}/result-${protocol}-${concurrency}.csv --plugin-type AggregateReport"
+  aggregateCommand="java -jar /opt/jmeter/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv ${jmeter_file_dest}/${jmeter_aggregate_file}.csv --input-jtl ${jmeter_file_dest}/${jmeter_result_file}.csv --plugin-type AggregateReport"
   echo "Run aggregate command: $aggregateCommand"
   run_ssh $privatekey_path ubuntu $egress_ip_address 2222 "$aggregateCommand"
 
   echo "Copy result files to local"
-  run_scp_local $privatekey_path ubuntu $egress_ip_address "${jmeter_file_dest}/aggregate-${protocol}-${concurrency}.csv" "/tmp/aggregate-${protocol}-${concurrency}.csv"
-  run_scp_local $privatekey_path ubuntu $egress_ip_address "${jmeter_file_dest}/result-${protocol}-${concurrency}.csv" "/tmp/result-${protocol}-${concurrency}.csv"
+  run_scp_local $privatekey_path ubuntu $egress_ip_address "${jmeter_file_dest}/${jmeter_aggregate_file}.csv" "${result_dir}/${jmeter_aggregate_file}.csv"
+  run_scp_local $privatekey_path ubuntu $egress_ip_address "${jmeter_file_dest}/${jmeter_result_file}.csv" "${result_dir}/${jmeter_result_file}.csv"
 }
 
 collect_result_jmeter()
