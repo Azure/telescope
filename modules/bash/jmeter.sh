ac93eaa252a4e4d5e139f36a7a5c4454097dbb18
alyssavu <alyssavu@microsoft.com>
2024-01-23 17:29:06 +0000

    update jmeter functions

--- modules/bash/jmeter.sh
+++ modules/bash/jmeter.sh
@@ -7,24 +7,23 @@ run_jmeter() {
   local egress_ip_address=$2
   local privatekey_path=$3
   local jmeter_file_dest=$4
-  local jmeter_properties=$5
-  local jmeter_result_file=$6
-  local jmeter_aggregate_file=$7
-  local jmeter_log_file=$8
-  local result_dir=$9
+  local jmeter_protocol=$5
+  local jmeter_concurrency=$6
+  local jmeter_properties=$7
+  local result_dir=$8
 
   # sleep 5m
-  jmeterCommand="jmeter -n -t ${jmeter_file_dest}/https_test.jmx -f -p ${jmeter_file_dest}/jmeter.properties -j ${jmeter_file_dest}/${jmeter_log_file}.log ${jmeter_properties}"
+  jmeterCommand="jmeter -n -t ${jmeter_file_dest}/https_test.jmx -f -S ${jmeter_file_dest}/jmeter.properties -j ${jmeter_file_dest}/jmeter-${protocol}-${concurrency}.log ${jmeter_properties}"
   echo "Run test command: $jmeterCommand"
   run_ssh $privatekey_path ubuntu $egress_ip_address 2222 "$jmeterCommand"
 
-  aggregateCommand="java -jar /opt/jmeter/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv ${jmeter_file_dest}/${jmeter_aggregate_file}.csv --input-jtl ${jmeter_file_dest}/${jmeter_result_file}.csv --plugin-type AggregateReport"
+  aggregateCommand="java -jar /opt/jmeter/lib/cmdrunner-2.2.jar --tool Reporter --generate-csv ${jmeter_file_dest}/aggregate-${protocol}-${concurrency}.csv --input-jtl ${jmeter_file_dest}/result-${protocol}-${concurrency}.csv --plugin-type AggregateReport"
   echo "Run aggregate command: $aggregateCommand"
   run_ssh $privatekey_path ubuntu $egress_ip_address 2222 "$aggregateCommand"
 
   echo "Copy result files to local"
-  run_scp_local $privatekey_path ubuntu $egress_ip_address 2222 "${jmeter_file_dest}/${jmeter_aggregate_file}.csv" "${result_dir}/${jmeter_aggregate_file}.csv"
-  run_scp_local $privatekey_path ubuntu $egress_ip_address 2222 "${jmeter_file_dest}/${jmeter_result_file}.csv" "${result_dir}/${jmeter_result_file}.csv"
+  run_scp_local $privatekey_path ubuntu $egress_ip_address 2222 "${jmeter_file_dest}/aggregate-${protocol}-${concurrency}.csv" "${result_dir}/aggregate-${protocol}-${concurrency}.csv"
+  run_scp_local $privatekey_path ubuntu $egress_ip_address 2222 "${jmeter_file_dest}/result-${protocol}-${concurrency}.csv" "${result_dir}/result-${protocol}-${concurrency}.csv"
 }
 
 collect_result_jmeter()
@@ -34,6 +33,7 @@ collect_result_jmeter()
   local result_dir=$3
   local run_id=$4
   local run_url=$5
+  local cloud_info=$6
 
   echo "Collect result for $protocol with $concurrency concurrency"
   result=$(cat "/tmp/aggregate-${protocol}-${concurrency}.csv" | python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))')
