2cb9fafdb8650ffff65d18eff79853c19dd7ebb7
Sumanth Reddy Chinna Pullaiah (from Dev Box) <schinnapulla@microsoft.com>
2024-01-31 15:17:59 -0500

    Allowing terraform inputs as JSON

--- modules/terraform/azure/onboarding/main.tf
+++ modules/terraform/azure/onboarding/main.tf
@@ -1,16 +1,9 @@
 locals {
-  tags = {
-    "owner"         = lookup(var.json_input, "owner", "github_actions")
-    "scenario"      = "${var.json_input.scenario_type}-${var.json_input.scenario_name}-${var.json_input.scenario_version}"
-    "creation_time" = timestamp()
-    "run_id"        = lookup(var.json_input, "run_id", "github_actions")
-  }
   formatted_scenario_name = "${var.json_input.scenario_name}-${var.json_input.scenario_version}"
 }
 provider "azurerm" {
   features {}
 }
-
 
 # Resource Group
 data "azurerm_resource_group" "rg" {
@@ -54,8 +47,16 @@ data "azurerm_eventhub_namespace" "eventhub_ns" {
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+data "azurerm_eventhub" "eventhub" {
+  count               = var.json_input.create_eventhub_instance ? 0 : 1
+  name                = var.json_input.eventhub_instance_name
+  namespace_name      = data.azurerm_eventhub_namespace.eventhub_ns.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
 resource "azurerm_eventhub" "eventhub" {
-  name                = local.formatted_scenario_name
+  count               = var.json_input.create_eventhub_instance ? 1 : 0
+  name                = var.json_input.eventhub_instance_name
   namespace_name      = data.azurerm_eventhub_namespace.eventhub_ns.name
   resource_group_name = data.azurerm_resource_group.rg.name
   partition_count     = 8
@@ -63,9 +64,9 @@ resource "azurerm_eventhub" "eventhub" {
 }
 
 resource "azurerm_eventhub_consumer_group" "consumer_group" {
-  name                = "default"
+  name                = local.formatted_scenario_name
   namespace_name      = data.azurerm_eventhub_namespace.eventhub_ns.name
-  eventhub_name       = azurerm_eventhub.eventhub.name
+  eventhub_name       = var.json_input.eventhub_instance_name
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
@@ -79,7 +80,7 @@ resource "azurerm_eventgrid_system_topic_event_subscription" "event_subscription
   system_topic          = data.azurerm_eventgrid_system_topic.topic.name
   resource_group_name   = data.azurerm_resource_group.rg.name
   event_delivery_schema = "EventGridSchema"
-  eventhub_endpoint_id  = azurerm_eventhub.eventhub.id
+  eventhub_endpoint_id  = var.json_input.create_eventhub_instance ? azurerm_eventhub.eventhub[0].id : data.azurerm_eventhub.eventhub[0].id
   included_event_types  = ["Microsoft.Storage.BlobCreated"]
   subject_filter {
     subject_begins_with = "/blobServices/default/containers/${var.json_input.scenario_type}/blobs/${var.json_input.scenario_name}/${var.json_input.scenario_version}"
@@ -95,11 +96,11 @@ resource "azurerm_kusto_eventgrid_data_connection" "evengrid_connection" {
   cluster_name                 = data.azurerm_kusto_cluster.cluster.name
   database_name                = data.azurerm_kusto_database.database.name
   storage_account_id           = data.azurerm_storage_account.storage.id
-  eventhub_id                  = azurerm_eventhub.eventhub.id
+  eventhub_id                  = var.json_input.create_eventhub_instance ? azurerm_eventhub.eventhub[0].id : data.azurerm_eventhub.eventhub[0].id
   eventhub_consumer_group_name = azurerm_eventhub_consumer_group.consumer_group.name
   managed_identity_resource_id = data.azurerm_kusto_cluster.cluster.id
   table_name                   = var.json_input.kusto_table_name
   data_format                  = "JSON"
   mapping_rule_name            = "${var.json_input.kusto_table_name}_mapping"
-  depends_on                   = [azurerm_eventgrid_system_topic_event_subscription.event_subscription]
+  depends_on                   = [azurerm_eventgrid_system_topic_event_subscription.event_subscription, azurerm_kusto_script.script]
 }
--- modules/terraform/azure/onboarding/variables.tf
+++ modules/terraform/azure/onboarding/variables.tf
@@ -1,18 +1,20 @@
 variable "json_input" {
   description = "value of the json input for onboarding automation"
   type = object({
-    owner                   = string
-    run_id                  = string
-    scenario_name           = string
-    scenario_type           = string
-    scenario_version        = string
-    resource_group_name     = string
-    storage_account_name    = string
-    kusto_cluster_name      = string
-    kusto_database_name     = string
-    kusto_table_name        = string
-    eventhub_namespace_name = string
-    eventgrid_topic_name    = string
-    table_creation_script   = string
+    owner                    = string
+    run_id                   = string
+    scenario_name            = string
+    scenario_type            = string
+    scenario_version         = string
+    resource_group_name      = string
+    storage_account_name     = string
+    kusto_cluster_name       = string
+    kusto_database_name      = string
+    kusto_table_name         = string
+    eventhub_namespace_name  = string
+    eventhub_instance_name   = string
+    create_eventhub_instance = bool
+    eventgrid_topic_name     = string
+    table_creation_script    = string
   })
 }
