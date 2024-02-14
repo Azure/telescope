1ada1fcd9dadb378b35a6a10b6e3af5b8733737e
Okechukwu Ogwo-Ude <okeyogwoude@gmail.com>
2024-02-14 09:11:30 -0800

    Deleted some unwanted files on this branch

--- scenarios/perf-eval/vm-iperf3/bash-scripts/client-userdata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo perl -pi -e 's/^#?Port 22$/Port 2222/' /etc/ssh/sshd_config
-sudo systemctl restart sshd
-
-sudo iptables -A INPUT -p tcp --dport 2222 -j ACCEPT
-
-sudo tdnf install iperf3 -y
