c54d511ccc7893ac16af4a2ca475c62d664a79fd
kevinjwa6112 <154457097+kevinjwa6112@users.noreply.github.com>
2024-02-13 20:39:13 -0500

    Adding new tests for zones (#63)
    Adding new tests for zones for tests 1a and 1b

---
+++ scenarios/perf-eval/vm-same-zone-iperf/bash-scripts/server-userdata.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo perl -pi -e 's/^#?Port 22$/Port 2222/' /etc/ssh/sshd_config
+sudo service ssh restart
+
+sudo apt-get update && sudo apt install iperf -y
+
+nohup iperf --server --port 20001 &> /dev/null &
+nohup iperf --server --udp --port 20002 &> /dev/null &
