4a0eeb487c668927d1dbb0c7c689d60c7a72d7be
ldb297 <ldbrooks297@gmail.com>
2024-03-11 16:15:00 -0700

    hardcode empty string to see deployment error

--- modules/terraform/azure/main.tf
+++ modules/terraform/azure/main.tf
@@ -235,5 +235,5 @@ module "private_endpoint" {
   pe_name                = var.pe_config.pe_name
   pe_subnet_id           = local.all_subnets[var.pe_config.pe_subnet_name]
 
-  resource_id = local.storage_account.id
+  resource_id = ""
 }
