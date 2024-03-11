134b11dc9fe3060904692ac879d8791d5eda8ae5
ldb297 <ldbrooks297@gmail.com>
2024-03-11 11:35:03 -0700

    fix pe subnet id

--- modules/terraform/azure/main.tf
+++ modules/terraform/azure/main.tf
@@ -233,5 +233,5 @@ module "private_endpoint" {
   location               = local.region
   
   pe_name                = var.pe_config.pe_name
-  pe_subnet_id           = var.network_config_list[0].subnet
+  pe_subnet_id           = local.all_subnets[var.pe_config.pe_subnet_name]
 }
