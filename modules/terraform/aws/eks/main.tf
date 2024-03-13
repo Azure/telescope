80b2e221209818abecd063c7108d0f88e9a83687
Sumanth Reddy CH <38838077+sumanthreddy29@users.noreply.github.com>
2024-03-13 19:57:56 -0400

    Add EKS module for AWS (#109)
    1. Adding EKS module to create EKS cluster, Managed Node groups and
    Addon's.
    2. Update git ignore to ignore pub extension files.
    3. Added Read me for EKS module.

---
+++ modules/terraform/aws/eks/main.tf
@@ -0,0 +1,101 @@
+locals {
+  eks_name           = var.eks_config.eks_name
+  eks_node_group_map = { for node_group in var.eks_config.eks_managed_node_groups : node_group.name => node_group }
+  eks_addons_map     = { for addon in var.eks_config.eks_addons : addon.name => addon }
+  policy_arns        = var.eks_config.policy_arns
+}
+
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "tag:run_id"
+    values = ["${var.run_id}"]
+  }
+
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com", "ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eks-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "policy_attachments" {
+  for_each = toset(local.policy_arns)
+
+  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# Create EKS Cluster
+resource "aws_eks_cluster" "eks" {
+  name     = local.eks_name
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = toset(data.aws_subnets.subnets.ids)
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.policy_attachments
+  ]
+
+  tags = var.tags
+}
+
+resource "aws_eks_node_group" "eks_managed_node_groups" {
+
+  for_each = local.eks_node_group_map
+
+  node_group_name = each.value.name
+  cluster_name    = aws_eks_cluster.eks.name
+  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+  subnet_ids      = toset(data.aws_subnets.subnets.ids)
+
+  scaling_config {
+    min_size     = each.value.min_size
+    max_size     = each.value.max_size
+    desired_size = each.value.desired_size
+  }
+
+  ami_type       = each.value.ami_type
+  instance_types = each.value.instance_types
+  capacity_type  = each.value.capacity_type
+  labels         = each.value.labels
+
+  tags = merge(var.tags, {
+    "Name" = each.value.name
+  })
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_iam_role_policy_attachment.policy_attachments
+  ]
+}
+
+module "eks_addon" {
+  source = "./addon"
+
+  count = length(var.eks_config.eks_addons) != 0 ? 1 : 0
+
+  eks_addon_config_map      = local.eks_addons_map
+  cluster_name              = aws_eks_cluster.eks.name
+  cluster_oidc_provider_url = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  tags                      = var.tags
+  depends_on                = [aws_eks_cluster.eks, aws_eks_node_group.eks_managed_node_groups]
+}
---
+++ modules/terraform/aws/virtual-network/output.tf
@@ -0,0 +1,4 @@
+output "vpc" {
+  description = "Vpc information"
+  value       = aws_vpc.vpc
+}
