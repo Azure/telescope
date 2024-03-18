ba94f8bf6bf35cefbeea2f996eb5225a3ef79733
ldb297 <ldbrooks297@gmail.com>
2024-03-18 12:37:36 -0700

    container access level

--- modules/terraform/azure/storage-container/main.tf
+++ modules/terraform/azure/storage-container/main.tf
@@ -3,5 +3,5 @@
 resource "azurerm_storage_container" "storage_container" {
   name                  = var.container_name
   storage_account_name  = var.storage_account_name
-  container_access_type = "anonymous"
+  container_access_type = "container"
 }
