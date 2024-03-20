8ed1c31555bb432f2f8f6c58cdc059d14ba2038b
ldb297 <ldbrooks297@gmail.com>
2024-03-20 14:42:08 -0700

    add tags to pe

--- modules/terraform/azure/private-endpoint/main.tf
+++ modules/terraform/azure/private-endpoint/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_private_endpoint" "private_endpoint" {
   name                = var.pe_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags = var.tags
 
   subnet_id = var.pe_subnet_id
 
