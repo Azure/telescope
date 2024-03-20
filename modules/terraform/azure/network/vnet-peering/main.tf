b7b13501a996ccb24ad5f5f16c86fb1e5c19520a
kevinjwa6112 <kezzwang@gmail.com>
2024-03-20 10:48:03 -0700

    fixing peering

--- modules/terraform/azure/network/vnet-peering/main.tf
+++ modules/terraform/azure/network/vnet-peering/main.tf
@@ -1,31 +1,43 @@
-data "azurerm_virtual_network" "client_vnet"{
-    name = "client_vnet"
-    resource_group_name = var.resource_group_name
+data "azurerm_virtual_network" "client_vnet" {
+  name                = "client-vnet"
+  resource_group_name = var.resource_group_name
 }
 
-output "client_vnet" {
-    value = data.azurerm_virtual_network.client_vnet
+output "client_vnet_id" {
+  value = data.azurerm_virtual_network.client_vnet.id
 }
 
-data "azurerm_virtual_network" "server_vnet"{
-    name = "server_vnet"
-    resource_group_name = var.resource_group_name
+output "client_vnet_name" {
+  value = data.azurerm_virtual_network.client_vnet.name
 }
 
-output "server_vnet" {
-    value = data.azurerm_virtual_network.server_vnet
+data "azurerm_virtual_network" "server_vnet" {
+  name                = "server-vnet"
+  resource_group_name = var.resource_group_name
+}
+
+output "server_vnet_id" {
+  value = data.azurerm_virtual_network.server_vnet.id
+}
+
+output "server_vnet_name" {
+  value = data.azurerm_virtual_network.server_vnet.name
 }
 
 resource "azurerm_virtual_network_peering" "to_client" {
-    name = "servertoclient"
-    resource_group_name = var.resource_group_name
-    virtual_network_name = server_vnet.name
-    remote_virtual_network_id = client_vnet.id
+  name                         = "servertoclient"
+  resource_group_name          = var.resource_group_name
+  virtual_network_name         = data.azurerm_virtual_network.server_vnet.name
+  remote_virtual_network_id    = data.azurerm_virtual_network.client_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
 }
 
 resource "azurerm_virtual_network_peering" "to_server" {
-    name = "clienttoserver"
-    resource_group_name = var.resource_group_name
-    virtual_network_name = client_vnet.name
-    remote_virtual_network_id = server_vnet.id
+  name                         = "clienttoserver"
+  resource_group_name          = var.resource_group_name
+  virtual_network_name         = data.azurerm_virtual_network.client_vnet.name
+  remote_virtual_network_id    = data.azurerm_virtual_network.server_vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic      = false
 }
