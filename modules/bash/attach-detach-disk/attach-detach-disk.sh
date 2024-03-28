604739e020ec599111512b080163052b51a35298
Alexandru Posea <aposea@microsoft.com>
2024-03-28 16:19:44 +0200

    Added tfvars and bash changes.

--- modules/bash/attach-detach-disk/attach-detach-disk.sh
+++ modules/bash/attach-detach-disk/attach-detach-disk.sh
@@ -5,6 +5,8 @@ operation_column="operation"
 disk_name_column="disk_name"
 time_column="time"
 result_column="result"
+time_unit_column="unit"
+timestamp_column="timestamp"
 
 # set Kusto database connection details
 kusto_cluster="your_kusto_cluster"
@@ -20,16 +22,27 @@ init_tests() {
     vm_name=$(get_vm_instance_by_name $run_id $role)
     disk_names=($(get_disk_instance_by_name $run_id $scenario_type $scenario_name))
 
+    # get VM operating system and size
+    vm_os=$(az vm show --name $vm_name --resource-group $resource_group --query "storageProfile.osDisk.osType" --output tsv)
+    vm_size=$(az vm show --name $vm_name --resource-group $resource_group --query "hardwareProfile.vmSize" --output tsv)
+
+    # retrieve disk sizes
+    disk_sizes=()
+    for disk_name in "${disk_names[@]}"; do
+        disk_size=$(az disk show --name $disk_name --resource-group $resource_group --query "diskSizeGb" --output tsv)
+        disk_sizes+=($disk_size)
+    done
+
     echo "Tests initialized. VM name: $vm_name, Disk names: ${disk_names[@]}"
 }
 
-#function to run disk test
-run_disk_test() {
+#function to measure attach operation
+measure_attach() {
     local disk_name=$1
+    local disk_size=$2
 
-    echo "Running tests for disk: $disk_name"
+    echo "Executing attach operation for disk: $disk_name"  > /dev/tty
     start_time=$(date +%s)
-    echo "Executing attach operation for disk: $disk_name"
     attach_result=$(attach_disk $vm_name $disk_name $resource_group)
     end_time=$(date +%s)
     if [ "$attach_result" == "success" ]; then
@@ -37,32 +50,95 @@ run_disk_test() {
     else
         attach_time=-1
     fi
-    attach_output="{\"$operation_column\": \"attach\", \"$disk_name_column\": \"$disk_name\", \"$time_column\": $attach_time, \"$time_unit_column\": \"seconds\", \"$result_column\": \"$attach_result\"}"
+
+    attach_output='{
+        "timestamp": "'$(date)'",
+        "region": "'$region'",
+        "operation_info": {
+            "operation": "attach",
+            "result": "'$attach_result'",
+            "time": '$attach_time',
+            "unit": "seconds"
+        },
+        "vm_info": {
+            "vm_name": "'$vm_name'",
+            "size": "'$vm_size'",
+            "os": "'$vm_os'"
+        },
+        "disk_info": {
+            "disk_name": "'$disk_name'",
+            "disk_size": "'$disk_size'"
+        },
+        "run_id": "'$run_id'"
+    }'
+
+    echo $attach_output
+}
+
+#function to measure detach operation
+measure_detach() {
+    local disk_name=$1
+    local disk_size=$2
+
+    echo "Executing detach operation for disk: $disk_name"  > /dev/tty
+    start_time=$(date +%s)
+    detach_result=$(detach_disk $vm_name $disk_name $resource_group)
+    end_time=$(date +%s)
+    if [ "$detach_result" == "success" ]; then
+        detach_time=$(($end_time - $start_time))
+    else
+        detach_time=-1
+    fi
+
+    detach_output='{
+        "timestamp": "'$(date)'",
+        "region": "'$region'",
+        "operation_info": {
+            "operation": "detach",
+            "result": "'$detach_result'",
+            "time": '$detach_time',
+            "unit": "seconds"
+        },
+        "vm_info": {
+            "vm_name": "'$vm_name'",
+            "size": "'$vm_size'",
+            "os": "'$vm_os'"
+        },
+        "disk_info": {
+            "disk_name": "'$disk_name'",
+            "disk_size": "'$disk_size'"
+        },
+        "run_id": "'$run_id'"
+    }'
+    echo $detach_output
+}
+
+#function to run disk test
+run_disk_test() {
+    local disk_name=$1
+    local disk_size=$2
+
+    echo "Running tests for disk: $disk_name"
+    attach_output=$(measure_attach $disk_name $disk_size)
     attach_filename="tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
     echo $attach_output > $attach_filename
-    echo "Attach operation result: $attach_result, time: $attach_time seconds"
+    echo "Attach operation result: $(echo $attach_output | jq -r .$result_column), time: $(echo $attach_output | jq -r .$time_column) seconds"
 
-    if [ "$attach_result" == "success" ]; then
-        start_time=$(date +%s)
-        echo "Executing detach operation for disk: $disk_name"
-        detach_result=$(detach_disk $vm_name $disk_name $resource_group)
-        end_time=$(date +%s)
-        if [ "$detach_result" == "success" ]; then
-            detach_time=$(($end_time - $start_time))
-        else
-            detach_time=-1
-        fi
-        detach_output="{\"$operation_column\": \"detach\", \"$disk_name_column\": \"$disk_name\", \"$time_column\": $detach_time, \"$time_unit_column\": \"seconds\", \"$result_column\": \"$detach_result\"}"
+    if [ "$(echo $attach_output | jq -r .$result_column)" == "success" ]; then
+        detach_output=$(measure_detach $disk_name $disk_size)
         detach_filename="tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
         echo $detach_output > $detach_filename
-        echo "Detach operation result: $detach_result, time: $detach_time seconds"
+        echo "Detach operation result: $(echo $detach_output | jq -r .$result_column), time: $(echo $detach_output | jq -r .$time_column) seconds"
     fi
 }
 
 #function to run tests
 run_tests() {
-    for disk_name in "${disk_names[@]}"; do
-        run_disk_test $disk_name &
+    for index in "${!disk_names[@]}"; do
+        disk_name="${disk_names[$index]}"
+        disk_size="${disk_sizes[$index]}"
+        run_disk_test $disk_name $disk_size &
+        wait
     done
     wait
 }
@@ -78,7 +154,7 @@ collect_results() {
 #function to collect results
 collect_results() {
     # merge all JSON files into one file
-    cat tmp/*.json > json_results
+    cat tmp/*.json > json_results.json
     echo "Results collected and merged into json_results file"
 }
 
@@ -106,7 +182,7 @@ cloud=$6
 
 source "./$cloud/utils.sh"
 
-#init_tests
-#run_tests
-#collect_results
-upload_results $ACCOUNT_NAME $ACCOUNT_KEY $scenario_type/$scenario_name v1 json_results
+init_tests
+run_tests
+collect_results
+#upload_results $ACCOUNT_NAME $ACCOUNT_KEY $scenario_type/$scenario_name v1 json_results
