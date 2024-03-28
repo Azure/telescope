01bd7e96df94c09bc5d6f33b5adcdeea515a34e3
ldb297 <ldbrooks297@gmail.com>
2024-03-28 08:24:59 -0700

    fmt output.tf

--- modules/terraform/azure/private-endpoint/output.tf
+++ modules/terraform/azure/private-endpoint/output.tf
@@ -1,4 +1,4 @@
 output "private_endpoint" {
-    description = "private endpoint"
-    value = azurerm_private_endpoint.private_endpoint
+  description = "private endpoint"
+  value       = azurerm_private_endpoint.private_endpoint
 }
