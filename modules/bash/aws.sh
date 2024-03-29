01cb9d0628e091b226e1248f6adaa10195c438a3
kevinjwa6112 <kezzwang@gmail.com>
2024-03-29 15:38:45 -0700

    moving script to the bash

--- modules/bash/aws.sh
+++ modules/bash/aws.sh
@@ -98,4 +98,62 @@ aws_lb_dns_name() {
 aws_create_vpc_peering(){
   local RUN_ID=$1
   local REGIONS=$2
+
+  # Step 1: Check for the VPC IDs of the Server and Client VPC 
+  for region in $(echo "$REGIONS" | jq -r '.[]'); do
+    echo "Checking for Server VPC in $region with run Id $RUN_ID"
+    temp_server_vpc_id=$(aws ec2 describe-vpcs --region $region --filters "Name=tag:Name,Values=server-vpc" "Name=tag:run_id,Values=$RUN_ID" --query "Vpcs[0].VpcId" --output text)
+    if [ ! -z "$temp_server_vpc_id" ] && [ "$temp_server_vpc_id" != "None" ]; then
+      server_vpc_id=$(aws ec2 describe-vpcs --region $region --filters "Name=tag:Name,Values=server-vpc" "Name=tag:run_id,Values=$RUN_ID" --query "Vpcs[0].VpcId" --output text)
+      server_vpc_region=$region
+    fi
+
+    echo "Checking for Client VPC in $region with run Id $RUN_ID"
+    temp_client_vpc_id=$(aws ec2 describe-vpcs --region $region --filters "Name=tag:Name,Values=client-vpc" "Name=tag:run_id,Values=$RUN_ID" --query "Vpcs[0].VpcId" --output text)
+    if [ ! -z "$temp_client_vpc_id" ] && [ "$temp_client_vpc_id" != "None" ]; then
+      client_vpc_id=$(aws ec2 describe-vpcs --region $region --filters "Name=tag:Name,Values=client-vpc" "Name=tag:run_id,Values=$RUN_ID" --query "Vpcs[0].VpcId" --output text)
+      client_vpc_region=$region
+    fi
+  done
+
+  echo "Server ID $server_vpc_id Client ID $client_vpc_id "
+
+  # Step 2: Create VPC peering connection between client and server VPCs
+  if [[ "$client_vpc_region" != "$server_vpc_region" ]]; then
+      echo "Creating VPC peering connection between $client_vpc_id ($client_vpc_region) and $server_vpc_id ($server_vpc_region)"
+
+      deletion_due_time=$(aws ec2 describe-vpcs --region $client_vpc_region --vpc-ids $client_vpc_id --query "Vpcs[*].Tags[?Key=='deletion_due_time'].Value" --output text)
+
+      peering_id=$(aws ec2 create-vpc-peering-connection \
+        --vpc-id $client_vpc_id \
+        --region $client_vpc_region \
+        --peer-vpc-id $server_vpc_id \
+        --peer-region $server_vpc_region \
+        --tag-specifications "ResourceType=vpc-peering-connection,Tags=[{Key=run_id,Value=${RUN_ID}},{Key=deletion_due_time,Value=${deletion_due_time}}]" \
+        --query "VpcPeeringConnection.VpcPeeringConnectionId" --output text)
+
+  else
+      echo "Error: Client and server VPCs are in the same region."
+  fi
+
+  # Wait until the peering connection is available
+  sleep 30 
+
+  # Step 3 Accept the Peering after it's been created. 
+  echo "Accepting Peering ID $peering_id"
+  aws ec2 accept-vpc-peering-connection --vpc-peering-connection-id $peering_id --region $server_vpc_region
+
+  # Step 4 Modify internet-rt Route Tables for both VPCs to go to the peering connection   
+  client_route_table=$(aws ec2 describe-route-tables --region $client_vpc_region --query "RouteTables[?VpcId=='$client_vpc_id' && Tags[?Key=='Name' && Value=='internet-rt']].RouteTableId" --output text)
+  server_route_table=$(aws ec2 describe-route-tables --region $server_vpc_region --query "RouteTables[?VpcId=='$server_vpc_id' && Tags[?Key=='Name' && Value=='internet-rt']].RouteTableId" --output text)
+
+  client_cidr_block=$(aws ec2 describe-vpcs --region $client_vpc_region --vpc-ids $client_vpc_id --query 'Vpcs[0].CidrBlock' --output text)
+  server_cidr_block=$(aws ec2 describe-vpcs --region $server_vpc_region --vpc-ids $server_vpc_id --query 'Vpcs[0].CidrBlock' --output text)
+
+  aws ec2 create-route --route-table-id $client_route_table --region $client_vpc_region --destination-cidr-block $server_cidr_block --vpc-peering-connection-id $peering_id
+  aws ec2 create-route --route-table-id $server_route_table --region $server_vpc_region --destination-cidr-block $client_cidr_block --vpc-peering-connection-id $peering_id
+
+  #Verify Route table is updated 
+  aws ec2 describe-route-tables --route-table-ids $client_route_table --region $client_vpc_region
+  aws ec2 describe-route-tables --route-table-ids $server_route_table --region $server_vpc_region
 }
