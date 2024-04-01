da52f967fbfbd0dc90e2e65ecd521cd60e48adbd
Alexandru Posea <aposea@microsoft.com>
2024-04-01 16:26:06 +0300

    Removed init method, added output_dir.

--- modules/bash/attach-detach-disk/attach-detach-disk.sh
+++ modules/bash/attach-detach-disk/attach-detach-disk.sh
@@ -1,50 +1,9 @@
 #!/bin/bash
 
-# set column names for JSON output
-operation_column="operation"
-disk_name_column="disk_name"
-time_column="time"
-result_column="result"
-time_unit_column="unit"
-timestamp_column="timestamp"
-
-# set Kusto database connection details
-kusto_cluster="your_kusto_cluster"
-kusto_database="your_kusto_database"
-kusto_table="your_kusto_table"
-
-#function to initialize tests
-init_tests() {
-    local run_id=$1
-    local scenario_type=$2
-    local scenario_name=$3
-
-    # create tmp directory if it does not exist
-    mkdir -p tmp
-
-    # get vm name and disk names
-    vm_name=$(get_vm_instance_by_name $run_id)
-    disk_names=($(get_disk_instance_by_name $run_id $scenario_type $scenario_name))
-
-    # get VM operating system and size
-    vm_os=$(az vm show --name $vm_name --resource-group $resource_group --query "storageProfile.osDisk.osType" --output tsv)
-    vm_size=$(az vm show --name $vm_name --resource-group $resource_group --query "hardwareProfile.vmSize" --output tsv)
-
-    # retrieve disk sizes
-    disk_sizes=()
-    for disk_name in "${disk_names[@]}"; do
-        disk_size=$(az disk show --name $disk_name --resource-group $resource_group --query "diskSizeGb" --output tsv)
-        disk_sizes+=($disk_size)
-    done
-
-    echo "Tests initialized. VM name: $vm_name, Disk names: ${disk_names[@]}"
-}
-
 # function to measure attach operation
 measure_attach() {
     local disk_name=$1
 
-    echo "Executing attach operation for disk: $disk_name"  > /dev/tty
     start_time=$(date +%s)
     attach_result=$(attach_disk $vm_name $disk_name $resource_group)
     end_time=$(date +%s)
@@ -85,7 +44,6 @@ measure_attach() {
 measure_detach() {
     local disk_name=$1
 
-    echo "Executing detach operation for disk: $disk_name"  > /dev/tty
     start_time=$(date +%s)
     detach_result=$(detach_disk $vm_name $disk_name $resource_group)
     end_time=$(date +%s)
@@ -127,13 +85,13 @@ run_disk_test() {
 
     echo "Running tests for disk: $disk_name"
     attach_output=$(measure_attach $disk_name)
-    attach_filename="tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
+    attach_filename="$output_dir/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
     echo $attach_output > $attach_filename
     echo "Attach operation result: $(echo $attach_output | jq -r .$result_column), time: $(echo $attach_output | jq -r .$time_column) seconds"
 
     if [ "$(echo $attach_output | jq -r .$result_column)" == "success" ]; then
         detach_output=$(measure_detach $disk_name)
-        detach_filename="tmp/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
+        detach_filename="$output_dir/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1).json"
         echo $detach_output > $detach_filename
         echo "Detach operation result: $(echo $detach_output | jq -r .$result_column), time: $(echo $detach_output | jq -r .$time_column) seconds"
     fi
@@ -156,10 +114,11 @@ execute()
     run_id=$1
     scenario_type=$2
     scenario_name=$3
-    resource_group=$run_id
+    export output_dir=$4
+    export resource_group=$run_id
 
     # create tmp directory if it does not exist
-    mkdir -p tmp
+    mkdir -p $output_dir
 
     # get vm name and disk names
     vm_name=$(get_vm_instance_by_name $run_id)
@@ -169,6 +128,8 @@ execute()
     vm_os=$(az vm show --name $vm_name --resource-group $resource_group --query "storageProfile.osDisk.osType" --output tsv)
     vm_size=$(az vm show --name $vm_name --resource-group $resource_group --query "hardwareProfile.vmSize" --output tsv)
 
+    region=$(az group show --name $resource_group --query "location" --output tsv)
+
     echo "Tests initialized. VM name: $vm_name, Disk names: ${disk_names[@]}"
 
     # Export variables
@@ -176,8 +137,8 @@ execute()
     export disk_names
     export vm_os
     export vm_size
+    export region
 
-    #init_tests $run_id $scenario_type $scenario_name
     run_tests
 }
 
@@ -209,17 +170,3 @@ upload_results() {
 
     echo "Results uploaded to Azure storage blob"
 }
-
-#main script
-#run_id=$1
-#scenario_type=$2
-#scenario_name=$3
-#cloud=$4
-#resource_group=$run_id
-
-#source "./$cloud/utils.sh"
-
-#init_tests
-#run_tests
-#collect_results
-#upload_results $ACCOUNT_NAME $ACCOUNT_KEY $scenario_type/$scenario_name v1 json_results
