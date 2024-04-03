455f97a8b73139620b193236b8c66d961db3c297
ldb297 <ldbrooks297@gmail.com>
2024-04-03 13:49:49 -0700

    add route table to vpc output

--- modules/terraform/aws/virtual-network/output.tf
+++ modules/terraform/aws/virtual-network/output.tf
@@ -2,3 +2,8 @@ output "vpc" {
   description = "Vpc information"
   value       = aws_vpc.vpc
 }
+
+output "route_tables" {
+  description = "Route tables associated with vpc"
+  value = list(aws_vpc.route_tables)
+}
