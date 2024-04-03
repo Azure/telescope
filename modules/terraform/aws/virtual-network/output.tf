584802511e72a8603b52738bb9807b02f4aa54d8
ldb297 <ldbrooks297@gmail.com>
2024-04-03 14:18:01 -0700

    fix route table name association

--- modules/terraform/aws/virtual-network/output.tf
+++ modules/terraform/aws/virtual-network/output.tf
@@ -5,5 +5,5 @@ output "vpc" {
 
 output "route_tables" {
   description = "Route tables associated with vpc"
-  value = { for route_table in aws_route_table.route_tables: route_table.Name => route_table.id }
+  value = { for route_table in aws_route_table.route_tables: route_table.tags.Name => route_table.id }
 }
