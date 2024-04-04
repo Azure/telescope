d42bc3d81f81f638488dfe031611c3f4764bf020
Alexandru Posea <aposea@microsoft.com>
2024-04-04 14:18:28 +0300

    Added comments, fixed some strings.

--- modules/bash/disk-attach-detach/azure.sh
+++ modules/bash/disk-attach-detach/azure.sh
@@ -1,36 +1,77 @@
-#function to get the vm instance from the environment by name
+#!/bin/bash
+
+#Description
+#   This script contains the functions to manage the resources in the resource group.
+#
+# Parameters:
+#   $1:  run_id: the ID of the test run (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+# 
+# Returns: name of the VM instance
+# Usage: get_vm_instance_by_name <run_id>
 get_vm_instance_by_name() {
     local run_id=$1
 
     echo  $(az resource list --resource-type Microsoft.Compute/virtualMachines --query "[?(tags.run_id == '$run_id')].name" --output tsv)
 }
 
-#function to get the disk instance from the environment by name
+#Description
+#   This function gets the disk instances by name.
+#
+# Parameters:
+#   $1:  run_id: the ID of the test run (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: name of the disk instances
+# Usage: get_disk_instances_by_name <run_id>
 get_disk_instances_by_name() {
     local run_id=$1
     
     echo  "$(az resource list --resource-type Microsoft.Compute/disks --query "[?(tags.run_id == '${run_id}')].name"  --output tsv)"
 }
 
-#function to attach disk to VM
+#Description
+#   This function ataches a disk to a vm.
+#
+# Parameters:
+#   $1:  vm_name: the name of the VM instance (e.g. vm-1)
+#   $2:  disk_name: the name of the disk instance (e.g. disk-1)
+#   $3:  resource_group: the name of the resource group (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: Error message of the operation otherwise nothing
+# Usage: attach_disk <vm_name> <disk_name> <resource_group>
 attach_disk() {
     local vm_name=$1
     local disk_name=$2
     local resource_group=$3
 
-    echo $(az vm disk attach -g $resource_group --vm-name ${vm_name} --name ${disk_name} 2>&1)
+    echo "$(az vm disk attach -g $resource_group --vm-name ${vm_name} --name ${disk_name} 2>&1)"
 }
 
-#function to detach disk to VM
+#Description
+#   This function detaches a disk from a vm.
+#
+# Parameters:
+#   $1:  vm_name: the name of the VM instance (e.g. vm-1)
+#   $2:  disk_name: the name of the disk instance (e.g. disk-1)
+#   $3:  resource_group: the name of the resource group (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: Error message of the operation otherwise nothing
+# Usage: detach_disk <vm_name> <disk_name> <resource_group>
 detach_disk() {
     local vm_name=$1
     local disk_name=$2
     local resource_group=$3
 
-    echo $(az vm disk detach -g $resource_group --vm-name ${vm_name} --name ${disk_name} 2>&1)
+    echo "$(az vm disk detach -g $resource_group --vm-name ${vm_name} --name ${disk_name} 2>&1)"
 }
 
-#function to validate the resources in the resource group
+#Description
+#   This function validates the resources in the resource group.
+#
+# Parameters:
+#   $1:  run_id: the ID of the test run (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: None
+# Usage: validate_resources <run_id>
 validate_resources() {
     local run_id=$1
     
@@ -53,14 +94,29 @@ validate_resources() {
     fi
 }
 
-#function that gets disk_name as parameter and returns the storage type and size of the disk
+#Description
+#   This function gets the storage type and size of a disk.
+#
+# Parameters:
+#   $1:  disk_name: the name of the disk instance (e.g. disk-1)
+#
+# Returns: JSON object containing the storage type and size of the disk
+# Usage: get_disk_storage_type_and_size <disk_name>
 get_disk_storage_type_and_size() {
     local disk_name=$1
 
     echo $(az disk list --query "[?name=='$disk_name'].{StorageType:sku.name, Size:diskSizeGB}" --output json)
 }
 
-# function to get the VM operating system
+#Description
+#   This function gets the operating system of a VM.
+#
+# Parameters:
+#   $1:  vm_name: the name of the VM instance (e.g. vm-1)
+#   $2:  resource_group: the name of the resource group (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: The operating system of the VM
+# Usage: get_vm_os <vm_name> <resource_group>
 get_vm_os() {
     local vm_name=$1
     local resource_group=$2
@@ -68,7 +124,15 @@ get_vm_os() {
     echo $(az vm show --name $vm_name --resource-group $resource_group --query "storageProfile.osDisk.osType" --output tsv)
 }
 
-# function to get the VM size
+#Description
+#   This function gets the size of a VM.
+#
+# Parameters:
+#   $1:  vm_name: the name of the VM instance (e.g. vm-1)
+#   $2:  resource_group: the name of the resource group (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: The size of the VM
+# Usage: get_vm_size <vm_name> <resource_group>
 get_vm_size() {
     local vm_name=$1
     local resource_group=$2
@@ -76,7 +140,14 @@ get_vm_size() {
     echo $(az vm show --name $vm_name --resource-group $resource_group --query "hardwareProfile.vmSize" --output tsv)
 }
 
-# function to get the region
+#Description
+#   This function gets the region of a resource group.
+#
+# Parameters:
+#   $1:  resource_group: the name of the resource group (e.g. c23f34-vf34g34g-3f34gf3gf4-fd43rf3f43)
+#
+# Returns: The region of the resource group
+# Usage: get_region <resource_group>
 get_region() {
     local resource_group=$1
         
