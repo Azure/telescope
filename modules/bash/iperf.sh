44c7bd0d49aad9cac7ca819f6330f527c1281005
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-04-08 21:57:15 +0000

    update iperf2

--- modules/bash/iperf.sh
+++ modules/bash/iperf.sh
@@ -60,15 +60,16 @@ run_iperf3() {
 run_iperf2() {
   local destination_ip_address=$1
   local client_public_ip_address=$2
-  local thread_mode=$3
-  local protocol=$4
-  local run_time=$5
-  local wait_time=$6
-  local privatekey_path=$7
-  local server_public_ip_address=$8
-  local result_dir=$9
-  local bandwidth_list_str=${10}
-  local jumpbox_public_ip_address=${11:-''}
+  local protocol=$3
+  local wait_time=$4
+  local privatekey_path=$5
+  local server_public_ip_address=$6
+  local result_dir=$7
+	local iperf_properties=$8
+	local bandwidth=$9
+  local jumpbox_public_ip_address=${10:-''}
+
+	echo "Run evaluation on $client_public_ip_address with destination ip $destination_ip_address and protocol $protocol and result path $result_dir"
 
   if [ -n "$jumpbox_public_ip_address" ]; then
     echo "Jumpbox public IP address is set to $jumpbox_public_ip_address, will test via jumpbox"
@@ -79,9 +80,9 @@ run_iperf2() {
 
   echo "Perform a draft run to warm up the vm"
   if [ "$protocol" = "udp" ]; then
-    command="iperf --enhancedreports --client $destination_ip_address --format m --time 30 --udp --port 20002"
+    command="iperf --enhancedreports $iperf_properties --format m  --udp --port 20002"
   else
-    command="iperf --enhancedreports --client $destination_ip_address --format m --time 30 --port 20001"
+    command="iperf --enhancedreports $iperf_properties --format m  --port 20001"
   fi
   if [ -z "$jumpbox_public_ip_address" ]; then
     echo "run_ssh $privatekey_path ubuntu $client_public_ip_address $command"
@@ -91,45 +92,28 @@ run_iperf2() {
     run_ssh_via_jumpbox $privatekey_path ubuntu $jumpbox_public_ip_address $client_public_ip_address 2222 "$command"
   fi
 
-  for bandwidth in $bandwidth_list_str
-  do
-    local command="iperf --enhancedreports --client $destination_ip_address --format m --time $run_time"
+	local command="iperf --enhancedreports $iperf_properties --format m"
 
-    if [ "$protocol" = "udp" ]; then
-      port=20002
-      command="$command --udp --port $port"
-    else
-      port=20001
-      command="$command --port $port"
-    fi
+	if [ "$protocol" = "udp" ]; then
+		port=20002
+	else
+		port=20001
+	fi
+	command="$command --port $port"
 
-    local parallel=1
-    local run_bandwidth=$bandwidth
-    if [ "$thread_mode" = "multi" ]; then
-      parallel=$(echo "$bandwidth / 1000" | bc)
-      if [ "$parallel" -eq 0 ]; then
-        parallel=1
-      else
-        run_bandwidth=1000
-      fi
-    fi
+	echo "Wait for 1 minutes before running"
+	sleep 60
 
-    command="$command --parallel $parallel --bandwidth ${run_bandwidth}M"
-
-    echo "Wait for 1 minutes before running"
-    sleep 60
-
-    if [ -z "$jumpbox_public_ip_address" ]; then
-      echo "run_ssh $privatekey_path ubuntu $client_public_ip_address $command"
-      run_ssh $privatekey_path ubuntu $client_public_ip_address 2222 "$command" > $result_dir/iperf2-${protocol}-${bandwidth}.log
-    else
-      echo "run_ssh_via_jumpbox $privatekey_path ubuntu $jumpbox_public_ip_address $client_public_ip_address $command"
-      run_ssh_via_jumpbox $privatekey_path ubuntu $jumpbox_public_ip_address $client_public_ip_address 2222 "$command" > $result_dir/iperf2-${protocol}-${bandwidth}.log
-    fi
-    # for debug
-    echo ======== iperf2-${protocol}-${bandwidth}.log ========
-    cat $result_dir/iperf2-${protocol}-${bandwidth}.log
-  done
+	if [ -z "$jumpbox_public_ip_address" ]; then
+		echo "run_ssh $privatekey_path ubuntu $client_public_ip_address $command"
+		run_ssh $privatekey_path ubuntu $client_public_ip_address 2222 "$command" > $result_dir/iperf2-${protocol}-${bandwidth}.log
+	else
+		echo "run_ssh_via_jumpbox $privatekey_path ubuntu $jumpbox_public_ip_address $client_public_ip_address $command"
+		run_ssh_via_jumpbox $privatekey_path ubuntu $jumpbox_public_ip_address $client_public_ip_address 2222 "$command" > $result_dir/iperf2-${protocol}-${bandwidth}.log
+	fi
+	# for debug
+	echo ======== iperf2-${protocol}-${bandwidth}.log ========
+	cat $result_dir/iperf2-${protocol}-${bandwidth}.log
 }
 
 collect_result_iperf3() {
@@ -154,7 +138,7 @@ collect_result_iperf3() {
       if echo "$iperf_info" | jq '.timestamp' > /dev/null; then
         timestamp=$(echo "$iperf_info" | jq -r '.timestamp')
       else
-	timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+				timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
       fi
 
       data=$(jq --null-input \
@@ -181,36 +165,30 @@ collect_result_iperf2() {
   local cloud_info=$4
   local run_id=$5
   local run_url=$6
-  local protocol_list_str=$7
-  local bandwidth_list_str=$8
+  local protocol=$7
+  local bandwidth=$8
 
   touch $result_dir/results.json
 
-  for protocol in $protocol_list_str
-  do
-    for bandwidth in $bandwidth_list_str
-    do
-      iperf_result="$result_dir/iperf2-${protocol}-${bandwidth}.log"
-      cat $iperf_result
-      iperf_info=$(python3 ./modules/python/iperf2/parser.py $protocol $iperf_result)
+	iperf_result="$result_dir/iperf2-${protocol}-${bandwidth}.log"
+	cat $iperf_result
+	iperf_info=$(python3 ./modules/python/iperf2/parser.py $protocol $iperf_result)
 
-      os_info="{}"
+	os_info="{}"
 
-      data=$(jq --null-input \
-        --arg timestamp "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
-        --arg metric "$protocol" \
-        --arg target_bw "$bandwidth" \
-        --arg unit "Mbits/sec" \
-        --arg iperf_info "$iperf_info" \
-        --arg os_info "$os_info" \
-        --arg cloud_info "$cloud_info" \
-        --arg egress_ip "$egress_ip_address" \
-        --arg ingress_ip "$ingress_ip_address" \
-        --arg run_id "$run_id" \
-        --arg run_url "$run_url" \
-        '{timestamp: $timestamp, metric: $metric, target_bandwidth: $target_bw, unit: $unit, iperf_info: $iperf_info, os_info: $os_info, cloud_info: $cloud_info, egress_ip: $egress_ip, ingress_ip: $ingress_ip, run_id: $run_id, run_url: $run_url}')
+	data=$(jq --null-input \
+		--arg timestamp "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+		--arg metric "$protocol" \
+		--arg target_bw "$bandwidth" \
+		--arg unit "Mbits/sec" \
+		--arg iperf_info "$iperf_info" \
+		--arg os_info "$os_info" \
+		--arg cloud_info "$cloud_info" \
+		--arg egress_ip "$egress_ip_address" \
+		--arg ingress_ip "$ingress_ip_address" \
+		--arg run_id "$run_id" \
+		--arg run_url "$run_url" \
+		'{timestamp: $timestamp, metric: $metric, target_bandwidth: $target_bw, unit: $unit, iperf_info: $iperf_info, os_info: $os_info, cloud_info: $cloud_info, egress_ip: $egress_ip, ingress_ip: $ingress_ip, run_id: $run_id, run_url: $run_url}')
 
-      echo $data >> $result_dir/results.json
-    done
-  done
+	echo $data >> $result_dir/results.json
 }
