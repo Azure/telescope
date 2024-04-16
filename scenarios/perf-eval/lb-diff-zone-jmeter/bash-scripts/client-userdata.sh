d41163c8c3474003b23f15db5c75a0366c53274d
alyssavu <alyssavu@microsoft.com>
2024-04-16 21:21:25 +0000

    update download link

--- scenarios/perf-eval/lb-diff-zone-jmeter/bash-scripts/client-userdata.sh
+++ scenarios/perf-eval/lb-diff-zone-jmeter/bash-scripts/client-userdata.sh
@@ -10,8 +10,8 @@ sudo apt-get install default-jre -y
 java -version
 
 # install jmeter
-jmeter_version="5.6.3"
-wget https://downloads.apache.org/jmeter/binaries/apache-jmeter-${jmeter_version}.tgz
+jmeter_version="5.6.2"
+wget https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-${jmeter_version}.tgz
 tar -xvf apache-jmeter-${jmeter_version}.tgz
 sudo mv apache-jmeter-${jmeter_version} /opt/jmeter
 sudo ln -s /opt/jmeter/bin/jmeter /usr/local/bin/jmeter
