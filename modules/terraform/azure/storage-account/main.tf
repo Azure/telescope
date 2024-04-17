5f0ca6bd1958a91fbd3275ffdd5e9b28229c4851
ldb297 <ldbrooks297@gmail.com>
2024-04-17 12:02:44 -0700

    add dependency and tags

--- modules/terraform/azure/storage-account/main.tf
+++ modules/terraform/azure/storage-account/main.tf
@@ -21,18 +21,22 @@ resource "azurerm_storage_share" "fileshare" {
   enabled_protocol     = var.storage_share_config.enabled_protocol
 }
 
-resource "azurerm_storage_container" "example" {
+resource "azurerm_storage_container" "storage_container" {
   count                 = var.storage_blob_config == null ? 0 : 1
   name                  = var.storage_blob_config.container_name
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
+  tags = var.tags
+  depends_on = azurerm_storage_account.storage_account
 }
 
-resource "azurerm_storage_blob" "blob" {
+resource "azurerm_storage_blob" "storage_blob" {
   count                  = var.storage_blob_config == null ? 0 : 1
   name                   = var.storage_blob_config.blob_name
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = var.storage_blob_config.container_name
   type                   = "Block"
   source                 = var.storage_blob_config.source_file_path
+  tags = var.tags
+  depends_on = azurerm_storage_container.storage_container
 }
