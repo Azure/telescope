a2a42b31a6086676b6769a0ae1bf92fe42aafc38
kevinjwa6112 <kezzwang@gmail.com>
2024-04-29 15:02:25 -0700

    update kv name

--- modules/terraform/azure/app-gateway/main.tf
+++ modules/terraform/azure/app-gateway/main.tf
@@ -117,7 +117,7 @@ resource "azurerm_application_gateway" "appgateway" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "agw" {
-  name                = "${var.run_id}-kv"
+  name                = "${var.resource_group_name}-kv"
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           =  data.azurerm_client_config.current.tenant_id
