cf9653f4e76601be13eeaa697e9be3da15f9f95f
kevinjwa6112 <kezzwang@gmail.com>
2024-04-29 14:55:17 -0700

    softdeletedats

--- modules/terraform/azure/app-gateway/main.tf
+++ modules/terraform/azure/app-gateway/main.tf
@@ -121,7 +121,7 @@ resource "azurerm_key_vault" "agw" {
   location            = var.location
   resource_group_name = var.resource_group_name
   tenant_id           =  data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days = 2
+  soft_delete_retention_days = 7
   purge_protection_enabled = false
   sku_name = "standard"
   tags = merge(
