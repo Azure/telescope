789090e7a70c011db9e61f6be530b4a53a9910a6
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-04-30 15:05:49 +0000

    update jq syntax

--- scenarios/issue-repro/lb-websocket-time-out-error/websocket/run-client.sh
+++ scenarios/issue-repro/lb-websocket-time-out-error/websocket/run-client.sh
@@ -7,13 +7,13 @@ if [[ $code -eq 0 ]]; then
   websocket_abnormal_closure_count=$(cat log.txt | grep -o 'websocket: close 1006 (abnormal closure)')
   jq --null-input \
     --arg websocket_duration_map "$websocket_duration_map" \
-    -- arg websocket_abnormal_closure_count "$websocket_abnormal_closure_count" \
+    --arg websocket_abnormal_closure_count "$websocket_abnormal_closure_count" \
     --arg server_address "$SERVER_ADDRESS" \
     --arg server_port "$SERVER_PORT" \
     --arg total_connections "$TOTAL_CONNECTIONS" \
     --arg parallel_connections "$PARALLEL_CONNECTIONS" \
     --arg client_timeout "$CLIENT_TIMEOUT" \
-    '{websocket_duration_map: $websocket_duration_map, websocket_abnormal_closure_count: $websocket_abnormal_closure_count ,server_address: $server_address, server_port: $server_port, total_connections: $total_connections, parallel_connections: $parallel_connections, client_timeout: $client_timeout}'
+    '{websocket_duration_map: $websocket_duration_map, websocket_abnormal_closure_count: $websocket_abnormal_closure_count,server_address: $server_address, server_port: $server_port, total_connections: $total_connections, parallel_connections: $parallel_connections, client_timeout: $client_timeout}'
 else
   echo "Client exited with error"
   cat log.txt
