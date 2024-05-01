a77c38c1ca068f65b6dc4ce9ea6aeae3b62f93b8
kevinjwa6112 <kezzwang@gmail.com>
2024-05-01 15:19:37 -0700

    updating trusted root

--- modules/terraform/azure/app-gateway/main.tf
+++ modules/terraform/azure/app-gateway/main.tf
@@ -76,6 +76,7 @@ resource "azurerm_application_gateway" "appgateway" {
       protocol                       = backend_http_settings.value.protocol
       request_timeout                = backend_http_settings.value.request_timeout
       probe_name                     = backend_http_settings.value.probe_name
+      trusted_root_certificate_names = backend_http_settings.value.protocol == "Https" ? ["self-signed-root"] : []
     }
   }
 
@@ -123,6 +124,6 @@ resource "azurerm_application_gateway" "appgateway" {
 
   trusted_root_certificate {
     name = "self-signed-root"
-    data = "LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tDQpNSUlCdHpDQ0FWMENGSEUvNk5mME92L3QxV2JCQlBTOWp2VlBJV0pOTUFvR0NDcUdTTTQ5QkFNQ01GNHhDekFKDQpCZ05WQkFZVEFrNU1NUTR3REFZRFZRUUlEQVZCZW5WeVpURVNNQkFHQTFVRUJ3d0pRVzF6ZEdWeVpHRnRNUkV3DQpEd1lEVlFRS0RBaHViR2xuYUhSbGJqRVlNQllHQTFVRUF3d1BjMlZzWm5OcFoyNWxaQzF5YjI5ME1CNFhEVEl6DQpNRFl5TWpFM05UQXpNMW9YRFRJME1EWXlNVEUzTlRBek0xb3dYakVMTUFrR0ExVUVCaE1DVGt3eERqQU1CZ05WDQpCQWdNQlVGNmRYSmxNUkl3RUFZRFZRUUhEQWxCYlhOMFpYSmtZVzB4RVRBUEJnTlZCQW9NQ0c1c2FXZG9kR1Z1DQpNUmd3RmdZRFZRUUREQTl6Wld4bWMybG5ibVZrTFhKdmIzUXdXVEFUQmdjcWhrak9QUUlCQmdncWhrak9QUU1CDQpCd05DQUFUTzZvVVpsRjBwRWdEME5nQ1Bsc1ptUjk2OVMrcHBzRlF1bVZFK1NYK1JkVDMwZ1BVRjFyRTB1WjZ2DQpLMWJRREhSSVV3bzNnZzJZTnZKb3BvbFVmL3VLTUFvR0NDcUdTTTQ5QkFNQ0EwZ0FNRVVDSVFDbDdlN1o0bHplDQoxTGowMS9zU1I2K0lCZHVESUpNTkQxamdsTTYvdDc0NXh3SWdZSHl3SjArNmw2SHgvT2tOTnlYZmxNalBvaWk0DQpoNHczNzQxNFZqMG56Qk09DQotLS0tLUVORCBDRVJUSUZJQ0FURS0tLS0tDQo="
+    key_vault_secret_id = data.azurerm_key_vault_certificate.Appgateway.secret_id
   }
 }
