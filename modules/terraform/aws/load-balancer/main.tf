43e38a7d059d08f2840b2a7a87803f9a3bdf8e7e
kevinjwa6112 <kezzwang@gmail.com>
2024-05-08 09:48:20 -0700

    another type

--- modules/terraform/aws/load-balancer/main.tf
+++ modules/terraform/aws/load-balancer/main.tf
@@ -5,6 +5,7 @@ locals {
     for tg in local.lb_target_group :
     "${tg.vpc_name}-${tg.tg_suffix}" => tg
   }
+  lb_vpc_name = var.loadbalancer_config.lb_target_group.vpc_name
 }
 
 data "aws_subnet" "subnets" {
@@ -38,7 +39,7 @@ resource "aws_lb" "nlb" {
 resource "aws_security_group" "alb_security_group" {
   name = "applbrules"
   description = "Allow inbound HTTP and HTTPS" 
-  vpc_id = local.lb_target_group.vpc_name
+  vpc_id = lb_vpc_name
   tags = merge(
     var.tags,
     {
