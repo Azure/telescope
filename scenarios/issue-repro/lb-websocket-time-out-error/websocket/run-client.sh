e718beaab920fd1db1fdf0723967a2743aaf4955
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-05-13 21:04:38 +0000

    Websocket-issue-repro

---
+++ scenarios/issue-repro/lb-websocket-time-out-error/websocket/run-client.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+./client &> log.txt
+code=$?
+if [[ $code -eq 0 ]]; then
+  websocket_duration_map=$(cat log.txt | grep -o '{.*}')
+  premature_closure_count=$(grep -oE 'Total number of premature closures: [0-9]+' log.txt | cut -d' ' -f6)
+  jq --null-input \
+    --arg websocket_duration_map "$websocket_duration_map" \
+    --arg premature_closure_count "$premature_closure_count" \
+    --arg server_address "$SERVER_ADDRESS" \
+    --arg server_port "$SERVER_PORT" \
+    --arg total_connections "$TOTAL_CONNECTIONS" \
+    --arg parallel_connections "$PARALLEL_CONNECTIONS" \
+    --arg client_timeout "$CLIENT_TIMEOUT" \
+    '{websocket_duration_map: $websocket_duration_map, premature_closure_count: $premature_closure_count,server_address: $server_address, server_port: $server_port, total_connections: $total_connections, parallel_connections: $parallel_connections, client_timeout: $client_timeout}'
+else
+  echo "Client exited with error"
+  cat log.txt
+fi
+exit $code
