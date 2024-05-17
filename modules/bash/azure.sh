bf4808892811665ac2a07c0946bb5924bf58f807
Shuv Pranjal <shuvpranjal@microsoft.com>
2024-05-17 11:57:27 +0530

    Added method to deploy sample app on virtual node

--- modules/bash/azure.sh
+++ modules/bash/azure.sh
@@ -198,15 +198,53 @@ azure_create_vnet_peering()
     done
 }
 
-azure_aks_enable_addons() {
+deploy_sample_app() {
   local resource_group=$1
   local aks_cluster=$2
   local addons=$3
   local subnet_name=$4
 
-  az aks enable-addons \
-    --resource-group $resource_group \
-    --name $aks_cluster \
-    --addons $addons \
-    --subnet-name $subnet_name
+  # Enable add-ons
+  azure_aks_enable_addons $resource_group $aks_cluster $addons $subnet_name
+
+  # Connect to cluster
+  azure_aks_get_credentials $resource_group $aks_cluster
+
+  # Create a file named virtual-node.yaml
+  cat << EOF > virtual-node.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: aci-helloworld
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: aci-helloworld
+  template:
+    metadata:
+      labels:
+        app: aci-helloworld
+    spec:
+      containers:
+      - name: aci-helloworld
+        image: mcr.microsoft.com/azuredocs/aci-helloworld
+        ports:
+        - containerPort: 80
+      nodeSelector:
+        kubernetes.io/role: agent
+        beta.kubernetes.io/os: linux
+        type: virtual-kubelet
+      tolerations:
+      - key: virtual-kubelet.io/provider
+        operator: Exists
+      - key: azure.com/aci
+        effect: NoSchedule
+EOF
+
+  # Run the application using the kubectl apply command.
+  kubectl apply -f virtual-node.yaml
+
+  # Get a list of pods and the scheduled node using the kubectl get pods command with the -o wide argument.
+  kubectl get pods -o wide
 }
