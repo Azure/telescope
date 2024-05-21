3186689981352ef90d27eeeab6c6a4ab34ad5142
Alyssa Vu <49544763+alyssa1303@users.noreply.github.com>
2024-05-21 14:17:07 -0400

    Update wrk to parse errors and unit (#180)
    Update wrk to parse when there are errors and separate data into value
    and unit
    Example run: [Pipeline
    link](https://dev.azure.com/msazure/CloudNativeCompute/_build/results?buildId=94102165&view=results)

--- modules/python/wrk/parser.py
+++ modules/python/wrk/parser.py
@@ -18,7 +18,8 @@ def parse_wrk_result(result_file):
         # Extracting test parameters
         test_params_match = re.search(r'Running (\S+) test @ (\S+)\n\s+(\d+) threads and (\d+) connections', result)
         if test_params_match:
-            parsed_result['duration'] = test_params_match.group(1)
+            duration, duration_unit = parse_unit(test_params_match.group(1))
+            parsed_result['duration'] = { 'value': duration, 'unit': duration_unit}
             parsed_result['url'] = test_params_match.group(2)
             parsed_result['threads'] = int(test_params_match.group(3))
             parsed_result['connections'] = int(test_params_match.group(4))
@@ -26,29 +27,42 @@ def parse_wrk_result(result_file):
         # Extracting thread stats
         latency_stats_match = re.search(r'Latency\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', result)
         if latency_stats_match:
+            avg, avg_unit = parse_unit(latency_stats_match.group(1))
+            stdev, stdev_unit = parse_unit(latency_stats_match.group(2))
+            maximum, max_unit = parse_unit(latency_stats_match.group(3))
+            within_stdev, within_stdev_unit = parse_unit(latency_stats_match.group(4))
             parsed_result['latency_stats'] = {
-                'avg': latency_stats_match.group(1),
-                'stdev': latency_stats_match.group(2),
-                'max': latency_stats_match.group(3),
-                'percent_within_stdev': latency_stats_match.group(4)
+                'avg': { 'value': avg, 'unit': avg_unit },
+                'stdev': { 'value': stdev, 'unit': stdev_unit },
+                'max': { 'value': maximum, 'unit': max_unit },
+                'within_stdev': { 'value': within_stdev, 'unit': within_stdev_unit }
             }
+
         req_sec_stats_match = re.search(r'Req/Sec\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)', result)
         if req_sec_stats_match:
+            avg, avg_unit = parse_unit(req_sec_stats_match.group(1))
+            stdev, stdev_unit = parse_unit(req_sec_stats_match.group(2))
+            maximum, max_unit = parse_unit(req_sec_stats_match.group(3))
+            within_stdev, within_stdev_unit = parse_unit(req_sec_stats_match.group(4))
             parsed_result['req_sec_stats'] = {
-                'avg': req_sec_stats_match.group(1),
-                'stdev': req_sec_stats_match.group(2),
-                'max': req_sec_stats_match.group(3),
-                'percent_within_stdev': req_sec_stats_match.group(4)
+                'avg': { 'value': avg, 'unit': avg_unit },
+                'stdev': { 'value': stdev, 'unit': stdev_unit },
+                'max': { 'value': maximum, 'unit': max_unit },
+                'within_stdev': { 'value': within_stdev, 'unit': within_stdev_unit }
             }
 
         # Extracting latency distribution
         latency_distribution_match = re.search(r'Latency Distribution\n\s+50%\s+(\S+)\n\s+75%\s+(\S+)\n\s+90%\s+(\S+)\n\s+99%\s+(\S+)', result)
         if latency_distribution_match:
+            per50, per50_unit = parse_unit(latency_distribution_match.group(1))
+            per75, per75_unit = parse_unit(latency_distribution_match.group(2))
+            per90, per90_unit = parse_unit(latency_distribution_match.group(3))
+            per99, per99_unit = parse_unit(latency_distribution_match.group(4))
             parsed_result['latency_distribution'] = {
-                '50th_percentile': latency_distribution_match.group(1),
-                '75th_percentile': latency_distribution_match.group(2),
-                '90th_percentile': latency_distribution_match.group(3),
-                '99th_percentile': latency_distribution_match.group(4)
+                '50th_percentile': { 'value': per50, 'unit': per50_unit },
+                '75th_percentile': { 'value': per75, 'unit': per75_unit },
+                '90th_percentile': { 'value': per90, 'unit': per90_unit },
+                '99th_percentile': { 'value': per99, 'unit': per99_unit }
             }
 
         # Extracting total requests
@@ -56,6 +70,22 @@ def parse_wrk_result(result_file):
         if total_requests_match:
             parsed_result['total_requests'] = int(total_requests_match.group(1))
         
+        # Extracting total read
+        total_read_match = re.search(r'(\S+) read', result)
+        if total_read_match:
+            value, unit = parse_unit(total_read_match.group(1))
+            parsed_result['total_read'] = { 'value': value, 'unit': unit }
+        
+        # Extracing socket errors
+        socket_errors_match = re.search(r'Socket errors: connect (\d+), read (\d+), write (\d+), timeout (\d+)', result)
+        if socket_errors_match:
+            parsed_result['socket_errors'] = {
+                'connect': int(socket_errors_match.group(1)),
+                'read': int(socket_errors_match.group(2)),
+                'write': int(socket_errors_match.group(3)),
+                'timeout': int(socket_errors_match.group(4))
+            }
+
         # Extracting requests/sec
         requests_sec_match = re.search(r'Requests/sec:\s+(\S+)', result)
         if requests_sec_match:
@@ -64,12 +94,30 @@ def parse_wrk_result(result_file):
         # Extracting transfer/sec
         transfer_match = re.search(r'Transfer/sec:\s+(\S+)', result)
         if transfer_match:
-            parsed_result['transfer_per_sec'] = transfer_match.group(1)
+            value, unit = parse_unit(transfer_match.group(1))
+            parsed_result['transfer_per_sec'] = { 'value': value, 'unit': unit }
 
         json_result = json.dumps(parsed_result)
         return json_result
     except FileNotFoundError:
         print("File not found!")
+        sys.exit(1)
+
+def parse_unit(value):
+    # Regular expression to match the number and the unit
+    pattern = r"([0-9]*\.?[0-9]+)([%a-zA-Z]+)"
+
+    # Perform the matching
+    match = re.match(pattern, value)
+
+    if match:
+        # Extract the number and unit
+        number = float(match.group(1))
+        unit = match.group(2)
+
+        return number, unit
+    else:
+        print(f"No match found for value {value}!")
         sys.exit(1)
 
 def main():
