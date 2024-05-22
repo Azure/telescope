bce3a3bc54fe271937984a00e2fe5d61214f25ca
Shuv Pranjal <shuvpranjal@microsoft.com>
2024-05-22 18:05:05 +0530

    updated vperf.sh

--- modules/bash/vperf.sh
+++ modules/bash/vperf.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Define the function to enable add-ons
-azure_aks_enable_addons() {
-  echo "Enable add-ons logic goes here"
+# Define the function to run kubectl commands
+run_kubectl_command() {
+    local command=$1
+
+    kubectlCommand="kubectl $command"
+    $kubectlCommand
 }
 
-# Define the function to get credentials
-azure_aks_get_credentials() {
-  echo "Get credentials logic goes here"
+# Define the function to enable add-ons and connect to cluster
+setup_cluster() {
+    local resource_group=$1
+    local aks_cluster=$2
+    local addons=$3
+    local subnet_name=$4
+
+    # Enable add-ons
+    azure_aks_enable_addons $resource_group $aks_cluster $addons $subnet_name
+
+    # Connect to cluster
+    azure_aks_get_credentials $resource_group $aks_cluster
 }
 
 # Define the function to deploy a sample app
 deploy_sample_app() {
-  local resource_group=$1
-  local aks_cluster=$2
-  local addons=$3
-  local subnet_name=$4
 
-  # Enable add-ons
-  azure_aks_enable_addons $resource_group $aks_cluster $addons $subnet_name
-
-  # Connect to cluster
-  azure_aks_get_credentials $resource_group $aks_cluster
-
-  # Create a file named virtual-node.yaml
-  cat << EOF > virtual-node.yaml
+    # Create a file named virtual-node.yaml
+    cat << EOF > virtual-node.yaml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -55,12 +57,29 @@ spec:
         effect: NoSchedule
 EOF
 
-  # Run the application using the kubectl apply command.
-  kubectl apply -f virtual-node.yaml
+    run_kubectl_command "apply -f virtual-node.yaml"
 
-  # Get a list of pods and the scheduled node using the kubectl get pods command with the -o wide argument.
-  kubectl get pods -o wide
+    run_kubectl_command "get pods -o wide"
 }
 
-# Call the function
-deploy_sample_app "resource_group_value" "aks_cluster_value" "addons_value" "subnet_name_value"
+# Define the function to setup cluster, deploy app and get pods
+setup_deploy_and_get_pods() {
+    local resource_group=$1
+    local aks_cluster=$2
+    local addons=$3
+    local subnet_name=$4
+
+    # Start the timer
+    start_time=$(date +%s)
+
+    # Setup cluster
+    setup_cluster $resource_group $aks_cluster $addons $subnet_name
+
+    # Deploy sample app
+    deploy_sample_app
+
+    # Stop the timer
+    end_time=$(date +%s)
+    execution_time=$(expr $end_time - $start_time)
+    echo "Deployment completed in $execution_time seconds"
+}
