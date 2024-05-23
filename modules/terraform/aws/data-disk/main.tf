ee39947ede61ab8254f26bea06b70c9fbd73688b
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-05-23 17:00:22 +0000

    add commented fix

--- modules/terraform/aws/data-disk/main.tf
+++ modules/terraform/aws/data-disk/main.tf
@@ -1,3 +1,7 @@
+# resource "aws_kms_key" "ebs_encryption" {
+#   enable_key_rotation = true
+# }
+
 resource "aws_ebs_volume" "data_disk" {
   availability_zone = var.zone
 
@@ -5,6 +9,10 @@ resource "aws_ebs_volume" "data_disk" {
   type       = var.data_disk_volume_type
   iops       = var.data_disk_iops_read_write
   throughput = var.data_disk_mbps_read_write
+
+  # encrypted  = true
+  # kms_key_id = aws_kms_key.ebs_encryption.id
+
 
   tags = var.tags
 }
