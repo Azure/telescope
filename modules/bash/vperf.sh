ca31a32175ac3c78dd24060ba3b3cb074cef7cd8
Shuv Pranjal <shuvpranjal@microsoft.com>
2024-05-29 17:29:40 +0530

    First draft with updated script and input files

--- modules/bash/vperf.sh
+++ modules/bash/vperf.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# Define the function to run kubectl commands
-run_kubectl_command() {
-    local command=$1
-
-    kubectlCommand="kubectl $command"
-    $kubectlCommand
-}
-
-# Define the function to enable add-ons and connect to cluster
-setup_cluster() {
+# Function to setup the cluster and create a pod
+execute_pod() {
     local resource_group=$1
     local aks_cluster=$2
     local addons=$3
     local subnet_name=$4
 
-    # Enable add-ons
-    azure_aks_enable_addons $resource_group $aks_cluster $addons $subnet_name
-}
+    # Function to enable add-ons and connect to cluster
+    setup_cluster() {
+        azure_aks_enable_addons $resource_group $aks_cluster $addons $subnet_name
+    }
 
-# Define the function to create a pod
-create_pod() {
-
-    # Create a file named virtual-node.yaml
-    cat << EOF > virtual-node.yaml
+    # Function to create a pod
+    create_pod() {
+        # Create a file named virtual-node.yaml
+        cat << EOF > virtual-node.yaml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -54,50 +46,35 @@ spec:
         effect: NoSchedule
 EOF
 
-    run_kubectl_command "apply -f virtual-node.yaml"
+        kubectl apply -f virtual-node.yaml
+    }
+
+    setup_cluster
+    create_pod
 }
 
-# Define the function to setup cluster, create pod and get pods
-setup_create_and_get_pods() {
-    local resource_group=$1
-    local aks_cluster=$2
-    local addons=$3
-    local subnet_name=$4
-    local namespace="default" # Pods will be created in the default namespace
-    local result_dir="results"
-    
-    # Create result directory if it doesn't exist
+# Function to collect results
+collect_result() {
+    local namespace="default"
+    local result_dir=$1
+    local run_url=$2
+
+    # Ensure the result directory exists
     mkdir -p $result_dir
-
-    # Setup cluster
-    setup_cluster $resource_group $aks_cluster $addons $subnet_name
-
-    # Create pod
-    create_pod
 
     # Measure the time it takes for the pod to reach the ready state
     start_time=$(kubectl -n ${namespace} get pods -o yaml | yq e '.items[].status.conditions[] | select(.type == "PodScheduled") | .lastTransitionTime' -)
     end_time=$(kubectl -n ${namespace} get pods -o yaml | yq e '.items[].status.conditions[] | select(.type == "Ready") | .lastTransitionTime' -)
-    execution_time=$(echo $(( $(date -d "$end_time" "+%s") - $(date -d "$start_time" "+%s") )))
+    execution_time=$(( $(date -d "$end_time" "+%s") - $(date -d "$start_time" "+%s") ))
     echo "Pod reached ready state in $execution_time seconds"
-    
+
     # Collect results
-    collect_results $result_dir $execution_time
-}
-
-# Define the function to collect results
-collect_results() {
-    local result_dir=$1
-    local execution_time=$2
-
     local timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
     local data=$(jq --null-input \
         --arg timestamp "$timestamp" \
         --arg execution_time "$execution_time" \
-        '{timestamp: $timestamp, execution_time: $execution_time}')
+        --arg run_url "$run_url" \
+        '{timestamp: $timestamp, execution_time: $execution_time, run_url: $run_url}')
 
     echo $data >> $result_dir/results.json
 }
-
-
-# setup_create_and_get_pods "your_resource_group" "your_aks_cluster" "your_addons" "your_subnet_name"
