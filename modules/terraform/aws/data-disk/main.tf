c22368813747abce07dab98a24e191d500027837
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-06-03 16:47:55 +0000

    test tfsec

--- modules/terraform/aws/data-disk/main.tf
+++ modules/terraform/aws/data-disk/main.tf
@@ -10,7 +10,7 @@ resource "aws_ebs_volume" "data_disk" {
   iops       = var.data_disk_iops_read_write
   throughput = var.data_disk_mbps_read_write
 
-  encrypted  = true
-  kms_key_id = aws_kms_key.ebs_encryption.id
-  tags       = var.tags
+  # encrypted  = true
+  # kms_key_id = aws_kms_key.ebs_encryption.id
+  tags = var.tags
 }
