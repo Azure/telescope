a4b7430762906b676ed1877bc71e55125790ba24
alyssavu <alyssavu@microsoft.com>
2024-06-05 22:17:53 +0000

    add parser for ncps

--- modules/bash/ncps/ncps.sh
+++ modules/bash/ncps/ncps.sh
@@ -31,7 +31,7 @@ set_up_ncps() {
 #   - $5: The result directory to store the result file
 #   - $6: The unique name of the raw wrk result file
 #
-# Usage: run_wrk <destination_ip> <client_ip> <wrk_options> <privatekey_path> <result_dir> <result_file_name>
+# Usage: run_ncps <destination_ip> <client_ip> <wrk_options> <privatekey_path> <result_dir> <result_file_name>
 run_ncps() {
   local destination_ip=$1
   local client_ip=$2
@@ -43,4 +43,34 @@ run_ncps() {
   ncpsCommand="ncps -c $destination_ip -sil $ncps_options"
   echo "Run test command: $ncpsCommand"
   run_ssh $privatekey_path ubuntu $client_ip 2222 "$ncpsCommand" > ${result_dir}/${result_file_name}.log
+}
+
+# Description:
+#   This function is used to collect the result of ncps
+#
+# Parameters:
+#   - $1: The result directory
+#   - $2: The unique name of the raw wrk result file
+#   - $3: The pipeline run URL
+#   - $4: The cloud information
+#
+# Usage: collect_ncps <result_dir> <result_file_name> <run_url> <cloud_info>
+collect_ncps() {
+  local result_dir=$1
+  local result_file_name=$2
+  local run_url=$3
+  local cloud_info=$4
+
+  result=$(python3 ./modules/python/ncps/parser.py ${result_dir}/${result_file_name}.log)
+  echo "wrk result: $result"
+
+  data=$(jq --null-input \
+    --arg timestamp "$(date -u +"%Y-%m-%dT%H:%M:%SZ")" \
+    --arg cloud_info "$cloud_info" \
+    --arg result "$result" \
+    --arg run_url "$run_url" \
+    '{timestamp: $timestamp, cloud_info: $cloud_info, result: $result, run_url: $run_url}')
+  
+  touch $result_dir/results.json
+  echo $data >> $result_dir/results.json
 }
