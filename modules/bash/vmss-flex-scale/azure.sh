e81780cf7decfff0a2afb391326167a956476a08
Dan Nimara <dannimara@microsoft.com>
2024-06-06 14:15:58 +0300

    Remove subnet parameter and duplicated method

--- modules/bash/vmss-flex-scale/azure.sh
+++ modules/bash/vmss-flex-scale/azure.sh
@@ -12,13 +12,12 @@
 #   - $6: resource_group: The resource group under which the VMSS was created (e.g. rg-my-vmss)
 #   - $7: network_security_group: The network security group (eg. my-nsg)
 #   - $8: vnet_name: The virtual network name (e.g. my-vnet)
-#   - $9: subnet: The subnet (e.g. my-subnet)
-#   - $10: security_type: [optional] The security type (e.g. TrustedLaunch)
-#   - $11: tags: [optional] The tags to use (e.g. "owner=azure_devops,creation_time=2024-03-11T19:12:01Z")
-#   - $12: admin_username: [optional] The admin username to use (e.g. my_username, default value is azureuser)
-#   - $13: admin_password: [optional] The admin password to use (e.g. my_password, default value is Azur3User!FTW)
+#   - $9: security_type: [optional] The security type (e.g. TrustedLaunch)
+#   - $10: tags: [optional] The tags to use (e.g. "owner=azure_devops,creation_time=2024-03-11T19:12:01Z")
+#   - $11: admin_username: [optional] The admin username to use (e.g. my_username, default value is azureuser)
+#   - $12: admin_password: [optional] The admin password to use (e.g. my_password, default value is Azur3User!FTW)
 #
-# Usage: create_vmss <vmss_name> <vm_size> <vm_os> <vm_instances> <region> <resource_group> <network_security_group> <vnet_name> <subnet> [security_type] [tags] [admin_username] [admin_password]
+# Usage: create_vmss <vmss_name> <vm_size> <vm_os> <vm_instances> <region> <resource_group> <network_security_group> <vnet_name> [security_type] [tags] [admin_username] [admin_password]
 create_vmss() {
     local vmss_name=$1
     local vm_size=$2
@@ -28,16 +27,15 @@ create_vmss() {
     local resource_group=$6
     local network_security_group=$7
     local vnet_name=$8
-    local subnet=$9
-    local security_type="${10:-"TrustedLaunch"}"
-    local tags="${11:-"''"}"
-    local admin_username="${12:-"azureuser"}"
-    local admin_password="${13:-"Azur3User!FTW"}"
+    local security_type="${9:-"TrustedLaunch"}"
+    local tags="${10:-"''"}"
+    local admin_username="${11:-"azureuser"}"
+    local admin_password="${12:-"Azur3User!FTW"}"
 
     az vmss create --name "$vmss_name" --resource-group "$resource_group" \
         --image "$vm_os" --vm-sku "$vm_size" --instance-count $vm_instances \
         --location "$region" --nsg "$network_security_group" --vnet-name "$vnet_name" \
-        --subnet "$subnet" --security-type "$security_type" --load-balancer "" --tags $tags \
+        --security-type "$security_type" --load-balancer "" --tags $tags \
         --admin-username "$admin_username" --admin-password "$admin_password" \
         -o json 2> "/tmp/$resource_group-$vmss_name-create_vmss-error.txt" > "/tmp/$resource_group-$vmss_name-create_vmss-output.txt"
 
@@ -48,7 +46,7 @@ create_vmss() {
         function _catch {
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
-            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}')
+            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         }
         trap _catch ERR
 
@@ -59,18 +57,18 @@ create_vmss() {
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
                 --argjson vmss_data "$vmss_data" \
-            '{succeeded: "true", vmss_name: $vmss_name, vmss_data: $vmss_data}')
+            '{succeeded: "true", vmss_name: $vmss_name, vmss_data: $vmss_data}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         else
             if [[ -n "$error" ]] && [[ "${error:0:8}" == "ERROR: {" ]]; then
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
                     --argjson vmss_data "${error:7}" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             else
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
                     --arg vmss_data "$error" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             fi
         fi
     )
@@ -83,16 +81,14 @@ create_vmss() {
 #   - $1: vmss_name: The name of the VMSS (e.g. my-vmss)
 #   - $2: resource_group: The resource group under which the VMSS was created (e.g. rg-my-vmss)
 #   - $3: vmss_capacity: The new VM capacity for the VMSS (e.g. 20)
-#   - $4: tags: [optional] The tags to use (e.g. "owner=azure_devops,creation_time=2024-03-11T19:12:01Z")
 #
-# Usage: scale_vmss <vmss_name> <resource_group> <vmss_capacity> <tags>
+# Usage: scale_vmss <vmss_name> <resource_group> <vmss_capacity>
 scale_vmss() {
     local vmss_name=$1
     local resource_group=$2
     local vmss_capacity=$3
-    local tags=${4:-"''"}
     
-    az vmss scale --name "$vmss_name" --new-capacity $vmss_capacity --resource-group "$resource_group" --tags $tags \
+    az vmss scale --name "$vmss_name" --new-capacity $vmss_capacity --resource-group "$resource_group" \
         -o json 2> "/tmp/$resource_group-$vmss_name-scale_vmss-error.txt" > "/tmp/$resource_group-$vmss_name-scale_vmss-output.txt"
 
     exit_code=$?
@@ -102,7 +98,7 @@ scale_vmss() {
         function _catch {
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
-            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}')
+            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         }
         trap _catch ERR
 
@@ -111,17 +107,17 @@ scale_vmss() {
         if [[ $exit_code -eq 0 ]]; then
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
-            '{succeeded: "true", vmss_name: $vmss_name}')
+            '{succeeded: "true", vmss_name: $vmss_name}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         else
             if [[ -n "$error" ]] && [[ "${error:0:8}" == "ERROR: {" ]]; then
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             else
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
                     --arg vmss_data "$error" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             fi
         fi
     )
@@ -149,7 +145,7 @@ delete_vmss() {
         function _catch {
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
-            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}')
+            '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: "Unknown error"}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         }
         trap _catch ERR
 
@@ -158,17 +154,17 @@ delete_vmss() {
         if [[ $exit_code -eq 0 ]]; then
             echo $(jq -c -n \
                 --arg vmss_name "$vmss_name" \
-            '{succeeded: "true", vmss_name: $vmss_name}')
+            '{succeeded: "true", vmss_name: $vmss_name}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
         else
             if [[ -n "$error" ]] && [[ "${error:0:8}" == "ERROR: {" ]]; then
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             else
                 echo $(jq -c -n \
                     --arg vmss_name "$vmss_name" \
                     --arg vmss_data "$error" \
-                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}')
+                '{succeeded: "false", vmss_name: $vmss_name, vmss_data: {error: $vmss_data}}') | sed -E 's/\\n|\\r|\\t|\\s| /\|/g'
             fi
         fi
     )
