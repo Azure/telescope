89efd3f3a62c8829a4e49f817ff7d13b1964f581
alyssavu <alyssavu@microsoft.com>
2024-06-07 22:42:22 +0000

    update userdata and remove source code

--- scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/client-userdata.sh
+++ scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/client-userdata.sh
@@ -4,13 +4,11 @@ set -e
 sudo perl -pi -e 's/^#?Port 22$/Port 2222/' /etc/ssh/sshd_config
 sudo service ssh restart
 
-sudo apt-get -qq update
-sudo apt-get -qq install gcc
-
-mkdir /home/ubuntu/ncps
-chown -R ubuntu:ubuntu /home/ubuntu/ncps
-
 sudo bash -c 'cat >> /etc/security/limits.conf' << EOF
 * soft nofile 1048575
 * hard nofile 1048575
 EOF
+
+wget https://telescopetools.z13.web.core.windows.net/packages/network-tools/ncps/ncps_1.1.tar.gz
+tar -xzf ncps_1.1.tar.gz
+cp ncps/ncps /bin/ncps
--- scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/server-userdata.sh
+++ scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/server-userdata.sh
@@ -7,10 +7,12 @@ sudo service ssh restart
 sudo apt-get -qq update
 sudo apt-get -qq install gcc
 
-mkdir /home/ubuntu/ncps
-chown -R ubuntu:ubuntu /home/ubuntu/ncps
-
 sudo bash -c 'cat >> /etc/security/limits.conf' << EOF
 * soft nofile 1048575
 * hard nofile 1048575
 EOF
+
+wget https://telescopetools.z13.web.core.windows.net/packages/network-tools/ncps/ncps_1.1.tar.gz
+tar -xzf ncps_1.1.tar.gz
+cp ncps/ncps /bin/ncps
+nohup ncps -s &> /dev/null &
