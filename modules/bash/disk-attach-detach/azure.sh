2ae13c2d2cbe9e8de60b42eadea3e78253e48d6e
Alexandru Posea <aposea@microsoft.com>
2024-06-17 14:56:57 +0300

    Revert "Debug!"
    This reverts commit 31ca6a9c78d033a4a1e3f37d13a4af5a525c05b1.

--- modules/bash/disk-attach-detach/azure.sh
+++ modules/bash/disk-attach-detach/azure.sh
@@ -78,8 +78,8 @@ attach_or_detach_disk() {
     then echo "Attached"; 
     else echo "Unattached"; fi)
     local external_polling_output_message="ERROR : Telescope polling timed out"
-    set -x
-    > "$filename"
+
+    echo -n "" > "$filename"
     internal_polling_result=""
 
     (
