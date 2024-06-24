710f203dc3339e4c378b7bbf7785c988ea47d72f
alyssavu <alyssavu@microsoft.com>
2024-06-24 18:21:06 +0000

    update userdata to modify settings

--- scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/client-userdata.sh
+++ scenarios/perf-eval/vm-same-zone-ncps/bash-scripts/client-userdata.sh
@@ -9,6 +9,20 @@ sudo bash -c 'cat >> /etc/security/limits.conf' << EOF
 * hard nofile 1048575
 EOF
 
+sudo bash -c 'cat >> /etc/rc.local' << EOF
+#!/bin/sh
+sysctl -w net.ipv4.tcp_tw_reuse=1 # TIME_WAIT work-around
+sysctl -w net.ipv4.ip_local_port_range="10000 60000" # ephemeral ports increased
+iptables -t raw -I OUTPUT -j NOTRACK # disable connection tracking
+iptables -t raw -I PREROUTING -j NOTRACK # disable connection tracking
+sysctl -w net.netfilter.nf_conntrack_max=0 # needed on some kernels
+sysctl -w net.ipv4.tcp_syncookies=0
+sysctl -w net.ipv4.tcp_max_syn_backlog=2048
+sysctl -w net.ipv4.conf.all.rp_filter=0
+sysctl -w fs.file-max=1048576
+EOF
+chmod +x /etc/rc.local
+
 wget https://telescopetools.z13.web.core.windows.net/packages/network-tools/ncps/ncps_1.1.tar.gz
 tar -xzf ncps_1.1.tar.gz
 cp ncps/ncps /bin/ncps
