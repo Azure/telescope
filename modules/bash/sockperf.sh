a4f0f38c7f5528d385bee082bdb69c98e000e1e3
mark xiong <haxiong@microsoft.com>
2024-07-01 17:27:19 +0000

    implement sockperf

---
+++ modules/bash/sockperf.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+source ./modules/bash/utils.sh
+
+run_sockperf() {
+    local egress_ip_address=$1
+    local user_name=$2
+    local ssh_port=$3
+    local privatekey_path=$4
+    local result_dir=$5
+    local protocol=$6
+    local sockperf_properties=$7
+
+    mkdir -p $result_dir
+    echo "Run evaluation on $egress_ip_address with user name $user_name at $ssh_port and ssh key $privatekey_path and result path $result_dir"
+
+    # sockperf ping-pong -i 10.2.1.122 -p 20004 -t 20 --tcp --pps=max --full-rtt
+    local command="sockperf $sockperf_properties --pps=max --full-rtt"
+
+    echo "Wait for 1 minutes before running"
+    sleep 10
+    echo "Run sockperf command: $command"
+    run_ssh $privatekey_path $user_name $egress_ip_address $ssh_port "$command" > $result_dir/sockperf-${protocol}.log
+}
+
+collect_result_sockperf() {
+  local result_dir=$1
+  local egress_ip_address=$2
+  local ingress_ip_address=$3
+  local cloud_info=$4
+  local run_id=$5
+  local protocol=$6
+  local bandwidth=$7
+
+  touch $result_dir/results.json
+
+  sockperf_result="$result_dir/sockperf-${protocol}.log"
+  cat $sockperf_result
+  sockperf_info=$(python3 ./modules/python/sockperf/parser.py $protocol $sockperf_result)
+  timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+  data=$(jq --null-input \
+    --arg timestamp "$timestamp" \
+    --arg metric "$protocol" \
+    --arg unit "usec" \
+    --arg sockperf_info "$sockperf_info" \
+    --arg cloud_info "$cloud_info" \
+    --arg egress_ip "$egress_ip_address" \
+    --arg ingress_ip "$ingress_ip_address" \
+    --arg run_id "$run_id" \
+    '{timestamp: $timestamp, metric: $metric, unit: $unit, sockperf_info: $sockperf_info, cloud_info: $cloud_info, egress_ip: $egress_ip, ingress_ip: $ingress_ip, run_id: $run_id}')
+
+  echo $data >> $result_dir/results.json
+}
---
+++ modules/python/sockperf/parser.py
@@ -0,0 +1,80 @@
+import sys
+import json
+import re
+
+def parse_sockperf_tcp_result(result_file, indent = None):
+  try:
+    file = open(result_file, 'r')
+    result = file.read()
+    file.close()
+    
+    if not result:
+        print("No data to process!")
+        sys.exit(1)
+    
+    parsed_result = {}
+
+    # Parse SentMessages
+    send_messages = re.search(r'SentMessages=(.+);', result)
+
+    # Parse Rtt and std-dev Rtt
+    rtt = re.search(r'> avg-rtt=(.+) ', result)
+    rtt_std = re.search(r' \(std-dev=(.+)\)', result)
+    if rtt:
+        parsed_result['rtt'] = rtt.group(1)
+    if rtt_std:
+        parsed_result['rtt_std'] = rtt_std.group(1)
+
+    # Parse percentiles
+    #Total 203820 observations; each percentile contains 2038.20 observations
+    percentiles_match = re.search(r' each percentile contains (.+) observations\n(.+?)<MIN>', result, re.DOTALL)
+    if percentiles_match:
+        percentiles_values = []
+        # parsed_result["test123"] = percentiles_match.group(2)
+        lines = percentiles_match.group(2).strip().split('\n')  # Skip the header line
+        for line in lines:
+            # <MAX> observation = 9229.894
+            #max_match = re.search(r'MAX(.+)', line.strip())
+
+            max_match = re.search(r' <MAX> observation = (.+)', line.strip())
+            if max_match:
+                percentiles_values.append({
+                    "SamplingType": "MAX",
+                    "Duration": float(max_match.group(1))
+                })
+            else:
+                not_max_match = re.search(r'sockperf: ---> percentile (.+) =  (.+)', line.strip())
+                if not_max_match:
+                    percentiles_values.append({
+                        "SamplingType": not_max_match.group(1),
+                        "Duration": float(not_max_match.group(2))
+                    })
+        parsed_result['percentiles'] = percentiles_values
+
+    # Convert to JSON
+    parsed_json = json.dumps(parsed_result, indent=indent)
+
+    return parsed_json
+    
+  except FileNotFoundError:
+    print("File not found!")
+    sys.exit(1)
+
+
+def parse_sockperf_udp_result(result_file):
+    print("Implement later")
+    parsed_result = {}
+    return parsed_result
+
+def main():
+  protocol = sys.argv[1]
+  fileName = sys.argv[2]
+
+  if protocol == 'tcp' or protocol == 'TCP':
+    result = parse_sockperf_tcp_result(fileName)
+  else:
+    result = parse_sockperf_udp_result(fileName)
+  print(result)
+
+if __name__ == '__main__':
+  main()
