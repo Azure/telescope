74ee3240f5c1a94a9a80ee2626561152f3cb1bf0
Dan Nimara <dannimara@microsoft.com>
2024-07-04 11:27:45 +0300

    change output redirect

--- modules/bash/vmss-flex-scale/utils.sh
+++ modules/bash/vmss-flex-scale/utils.sh
@@ -178,7 +178,7 @@ measure_create_vmss() {
         ;;
         aws)
             vmss_data=$(create_asg "$vmss_name" "$vm_instances" "$vm_scale_instances_target" "$lt_name" "$region" "$tags")
-            wait_for_desired_capacity $vmss_name "$vm_instances"
+            wait_for_desired_capacity $vmss_name "$vm_instances" 2>/dev/null
         ;;
         gcp)
             # GCP Method call
@@ -265,7 +265,7 @@ measure_scale_vmss() {
         ;;
         aws)
             vmss_data=$(scale_asg "$vmss_name" "$new_capacity")
-            wait_for_desired_capacity $vmss_name "$new_capacity"
+            wait_for_desired_capacity $vmss_name "$new_capacity" 2>/dev/null
         ;;
         gcp)
             # GCP Method call
