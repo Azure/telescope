4eecdf16e55d818d574ecb13edc95724cc4d5064
Dan Nimara <dannimara@microsoft.com>
2024-07-05 12:24:56 +0300

    scale

--- modules/bash/vmss-flex-scale/utils.sh
+++ modules/bash/vmss-flex-scale/utils.sh
@@ -265,7 +265,6 @@ measure_scale_vmss() {
         ;;
         aws)
             vmss_data=$(scale_asg "$vmss_name" "$new_capacity")
-            wait_for_desired_capacity "$vmss_name" "$new_capacity"
             wait_for_scaling_activities "$vmss_name"
         ;;
         gcp)
