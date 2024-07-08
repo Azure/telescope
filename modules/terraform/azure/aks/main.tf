b7fd904298240e40b2467d8eeb65df226a3960f1
Shuv Pranjal <shuvpranjal@microsoft.com>
2024-07-08 23:30:17 +0530

    added helm-release

--- modules/terraform/azure/aks/main.tf
+++ modules/terraform/azure/aks/main.tf
@@ -77,4 +77,5 @@ resource "helm_release" "vn2" {
   chart      = "https://shuvstorageaccount.blob.core.windows.net/mycontainer/virtualnode2-0.0.1.tgz"
 
   provider = helm
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
