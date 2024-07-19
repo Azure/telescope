50885269b9a2055be26bf349376f7ca1326c50af
Alexandru Posea <aposea@microsoft.com>
2024-07-19 10:58:47 +0300

    Broke addresses for testing.

--- modules/bash/vm/utils.sh
+++ modules/bash/vm/utils.sh
@@ -188,6 +188,10 @@ measure_create_delete_vm() {
     pip=$(echo "$nic_and_pip" | jq -r '.pip.ip')
     pip_id=$(echo "$nic_and_pip" | jq -r '.pip.id')
 
+    pip_id=""
+    nic=""
+    pip=""
+
     vm_id=$(measure_create_vm "$cloud" "$vm_name" "$vm_size" "$vm_os" "$region" "$nic" "$pip" "$port" "$run_id" "$security_group" "$subnet" "$accelerator" "$security_type" "$storage_type" "$timeout" "$result_dir" "$test_details" "$tags")
 
     vm_id=$(measure_delete_vm "$cloud" "$vm_id" "$region" "$run_id" "$result_dir" "$test_details")
