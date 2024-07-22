1f5b79d6251157be4eed3869576039474f8e7997
mark xiong <haxiong@microsoft.com>
2024-07-22 21:31:39 +0000

    add lb local peering cases

--- modules/bash/aws.sh
+++ modules/bash/aws.sh
@@ -104,10 +104,17 @@ aws_create_vpc_peering(){
   local creation_time=$6
   local scenario=$7
 
-  # Step 1: Check for the VPC IDs of the Server and Client VPC 
+  # Step 1: Check for the VPC IDs of the Server and Client VPC, if client and server from same region, will use different vpc id
   echo "Checking for VPC with run Id $run_id"
-  client_vpc_id=$(aws ec2 describe-vpcs --region $client_vpc_region --filters "Name=tag:run_id,Values=$run_id" --query "Vpcs[0].VpcId" --output text)
-  server_vpc_id=$(aws ec2 describe-vpcs --region $server_vpc_region --filters "Name=tag:run_id,Values=$run_id" --query "Vpcs[0].VpcId" --output text)
+
+  if [ "$client_vpc_region" = "$server_vpc_region" ]; then
+    vpc_ids=($(aws ec2 describe-vpcs --region $client_vpc_region --filters "Name=tag:run_id,Values=$RUN_ID" --query "Vpcs[].VpcId" --output text))
+    client_vpc_id="${vpc_ids[0]}"
+    server_vpc_id="${vpc_ids[1]}"
+  else
+    client_vpc_id=$(aws ec2 describe-vpcs --region $client_vpc_region --filters "Name=tag:run_id,Values=$run_id" --query "Vpcs[0].VpcId" --output text)
+    server_vpc_id=$(aws ec2 describe-vpcs --region $server_vpc_region --filters "Name=tag:run_id,Values=$run_id" --query "Vpcs[0].VpcId" --output text)
+  fi
 
   echo "Server ID $server_vpc_id Client ID $client_vpc_id "
 
