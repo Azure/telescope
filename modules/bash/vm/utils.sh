37ae8902a68a8b2f887441deff5f36d3c026b031
Alexandru Posea <aposea@microsoft.com>
2024-07-22 15:18:28 +0300

    Removed white spaces.

--- modules/bash/vm/utils.sh
+++ modules/bash/vm/utils.sh
@@ -182,7 +182,7 @@ measure_create_delete_vm() {
 - Storage type: $storage_type
 - Timeout: $timeout
 - Tags: $tags"
-    
+
     nic_and_pip=$(precreate_nic_and_pip_if_needed "$cloud" "$vm_name" "$run_id" "$region" "$security_group" "$subnet" "$tags" "$precreate_nic")
     nic=$(echo "$nic_and_pip" | jq -r '.nic')
     pip=$(echo "$nic_and_pip" | jq -r '.pip.ip')
