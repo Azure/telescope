5bd12755315cf444a95a5880aa7c1e59ee7f3507
kevinjwa6112 <kezzwang@gmail.com>
2024-07-30 14:27:08 -0700

    another typo

--- modules/terraform/azure/proximity-placement-group/main.tf
+++ modules/terraform/azure/proximity-placement-group/main.tf
@@ -1,5 +1,4 @@
 resource "azurerm_proximity_placement_group" "placement_group" {
-  count               = var.proximity_placement ? 1 : 0
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
