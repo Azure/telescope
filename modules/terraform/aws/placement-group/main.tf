30aa6b0fb449e7e67640a29855ac672f03f50d69
ldb297 <ldbrooks297@gmail.com>
2024-07-30 17:00:50 -0700

    update placement group name to run id

--- modules/terraform/aws/placement-group/main.tf
+++ modules/terraform/aws/placement-group/main.tf
@@ -1,5 +1,5 @@
 resource "aws_placement_group" "placement-group" {
-  name            = var.pg_config.name
+  name            = locals.run_id
   strategy        = var.pg_config.strategy
   tags            = var.tags
   partition_count = var.pg_config.partition_count
