618c3fe7748ac41ee3be1d7a4b2f21c62e82a488
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-08-08 21:16:36 +0000

    fix lint errors

--- modules/terraform/azure/aks/variables.tf
+++ modules/terraform/azure/aks/variables.tf
@@ -16,22 +16,10 @@ variable "tags" {
   }
 }
 
-variable "subnet_id" {
-  description = "Subnet ID"
-  type        = string
-  default     = ""
-}
-
 variable "vnet_id" {
   description = "Vnet id"
   type        = string
   default     = ""
-}
-
-variable "subnets" {
-  description = "Maps of subnets"
-  type        = map(string)
-  default     = {}
 }
 
 variable "aks_config" {
