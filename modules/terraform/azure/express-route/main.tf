c0c28f812dc427da7153b6b463df473610b022fd
kevinjwa6112 <kezzwang@gmail.com>
2024-08-29 16:22:40 -0700

    gatewayt config

--- modules/terraform/azure/express-route/main.tf
+++ modules/terraform/azure/express-route/main.tf
@@ -15,12 +15,12 @@ resource "azurerm_virtual_network_gateway" "vnet_gateway" {
 }
 
 resource "azurerm_virtual_network_gateway_connection" "onpremise" {
-  name                = var.vnet_gateway_connection_config.connection_name
+  name                = var.vnet_gateway_config.vnet_gateway_connection.connection_name
   location            = var.location
   resource_group_name = var.resource_group_name
   depends_on          = [azurerm_virtual_network_gateway.vnet_gateway]
 
-  type                       = var.vnet_gateway_connection_config.type
+  type                       = var.vnet_gateway_config.vnet_gateway_connection.type
   virtual_network_gateway_id = azurerm_virtual_network_gateway.vnet_gateway.id
 
   tags = var.tags
