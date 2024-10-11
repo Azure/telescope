30b24ed649d474eac9a1f429570f15ccb9e431a8
jshr-w <shjayaraman@microsoft.com>
2024-10-11 16:47:06 -0700

    init commit: test outline

---
+++ modules/python/clusterloader2/cilscale/cilscale.py
@@ -0,0 +1,104 @@
+import json
+import os
+import argparse
+
+from datetime import datetime, timezone
+from utils import parse_xml_to_json, run_cl2_command
+
+def override_config_clusterloader2(override_file):
+    pass
+
+def execute_clusterloader2(cl2_image, cl2_config_dir, cl2_report_dir, kubeconfig, provider):
+    run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, overrides=True)
+
+def collect_clusterloader2(
+    cl2_report_dir,
+    cloud_info,
+    run_id,
+    run_url,
+    result_file
+):
+    details = parse_xml_to_json(os.path.join(cl2_report_dir, "junit.xml"), indent = 2)
+    json_data = json.loads(details)
+    testsuites = json_data["testsuites"]
+
+    if testsuites:
+        status = "success" if testsuites[0]["failures"] == 0 else "failure"
+    else:
+        raise Exception(f"No testsuites found in the report! Raw data: {details}")
+
+    template = {
+        "timestamp": datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        "status": status,
+        "measurement": None,
+        "result": None,
+        "test_details": details,
+        "cloud_info": cloud_info,
+        "run_id": run_id,
+        "run_url": run_url
+    }
+    content = ""
+    for f in os.listdir(cl2_report_dir):
+        file_path = os.path.join(cl2_report_dir, f)
+        with open(file_path, 'r') as f:
+            measurement = parse_file(file_path)
+            data = json.loads(f.read())
+
+            if "dataItems" in data:
+                items = data["dataItems"]
+                for item in items:
+                    result = template.copy()
+                    result["measurement"] = measurement
+                    result["result"] = item
+                    content += json.dumps(result) + "\n"
+            else:
+                result = template.copy()
+                result["measurement"] = measurement
+                result["result"] = data
+                content += json.dumps(result) + "\n"
+
+    os.makedirs(os.path.dirname(result_file), exist_ok=True)
+    with open(result_file, 'w') as f:
+        f.write(content)
+
+def parse_file(fpath):
+    f = os.path.basename(fpath)
+    if f.startswith("GenericPrometheusQuery"):
+        return f.split(" ")[0]
+    return f.split("_")[0]
+
+def main():
+    parser = argparse.ArgumentParser(description="Cilscale Kubernetes resources.")
+    subparsers = parser.add_subparsers(dest="command")
+
+    # Sub-command for override_config_clusterloader2
+    parser_override = subparsers.add_parser("override", help="Override CL2 config file")
+    parser_override.add_argument("cl2_override_file", type=str, help="Path to the overrides of CL2 config file")
+
+    # Sub-command for execute_clusterloader2
+    parser_execute = subparsers.add_parser("execute", help="Execute scale up operation")
+    parser_execute.add_argument("cl2_image", type=str, help="Name of the CL2 image")
+    parser_execute.add_argument("cl2_config_dir", type=str, help="Path to the CL2 config directory")
+    parser_execute.add_argument("cl2_report_dir", type=str, help="Path to the CL2 report directory")
+    parser_execute.add_argument("kubeconfig", type=str, help="Path to the kubeconfig file")
+    parser_execute.add_argument("provider", type=str, help="Cloud provider name")
+
+    # Sub-command for collect_clusterloader2
+    parser_collect = subparsers.add_parser("collect", help="Collect scale up data")
+    parser_collect.add_argument("cl2_report_dir", type=str, help="Path to the CL2 report directory")
+    parser_collect.add_argument("cloud_info", type=str, help="Cloud information")
+    parser_collect.add_argument("run_id", type=str, help="Run ID")
+    parser_collect.add_argument("run_url", type=str, help="Run URL")
+    parser_collect.add_argument("result_file", type=str, help="Path to the result file")
+
+    args = parser.parse_args()
+
+    if args.command == "override":
+        override_config_clusterloader2(args.cl2_override_file)
+    elif args.command == "execute":
+        execute_clusterloader2(args.cl2_image, args.cl2_config_dir, args.cl2_report_dir, args.kubeconfig, args.provider)
+    elif args.command == "collect":
+        collect_clusterloader2(args.cl2_report_dir, args.cloud_info, args.run_id, args.run_url, args.result_file)
+
+if __name__ == "__main__":
+    main()
--- modules/python/clusterloader2/utils.py
+++ modules/python/clusterloader2/utils.py
@@ -8,14 +8,17 @@ from docker_client import DockerClient
 def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, overrides=False):
     docker_client = DockerClient()
 
+    # TODO: cp ./deployment-churn/additional-measurements.yaml ./testing/load
+
     command=f"""--provider={provider} --v=2 --enable-exec-service=false
---kubeconfig /root/.kube/config 
---testconfig /root/perf-tests/clusterloader2/config/config.yaml 
---report-dir /root/perf-tests/clusterloader2/results"""
+--kubeconfig /root/.kube/config
+--testconfig /root/perf-tests/clusterloader2/config/config.yaml
+--report-dir /root/perf-tests/clusterloader2/results
+--enable-prometheus-server=true"""
     if overrides:
         command += f" --testoverrides=/root/perf-tests/clusterloader2/config/overrides.yaml"
 
-    volumes = { 
+    volumes = {
         kubeconfig: {'bind': '/root/.kube/config', 'mode': 'rw'},
         cl2_config_dir: {'bind': '/root/perf-tests/clusterloader2/config', 'mode': 'rw'},
         cl2_report_dir: {'bind': '/root/perf-tests/clusterloader2/results', 'mode': 'rw'}
@@ -36,13 +39,13 @@ def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provi
 def parse_xml_to_json(file_path, indent = 0):
     with open(file_path, 'r') as file:
         xml_content = file.read()
-    
+
     dom = minidom.parseString(xml_content)
-    
+
     result = {
         "testsuites": []
     }
-    
+
     # Extract test suites
     testsuites = dom.getElementsByTagName("testsuite")
     for testsuite in testsuites:
@@ -50,7 +53,7 @@ def parse_xml_to_json(file_path, indent = 0):
         suite_tests = int(testsuite.getAttribute("tests"))
         suite_failures = int(testsuite.getAttribute("failures"))
         suite_errors = int(testsuite.getAttribute("errors"))
-        
+
         suite_result = {
             "name": suite_name,
             "tests": suite_tests,
@@ -58,31 +61,31 @@ def parse_xml_to_json(file_path, indent = 0):
             "errors": suite_errors,
             "testcases": []
         }
-        
+
         # Extract test cases
         testcases = testsuite.getElementsByTagName("testcase")
         for testcase in testcases:
             case_name = testcase.getAttribute("name")
             case_classname = testcase.getAttribute("classname")
             case_time = testcase.getAttribute("time")
-            
+
             case_result = {
                 "name": case_name,
                 "classname": case_classname,
                 "time": case_time,
                 "failure": None
             }
-            
+
             # Check for failure
             failure = testcase.getElementsByTagName("failure")
             if failure:
                 failure_message = failure[0].firstChild.nodeValue
                 case_result["failure"] = failure_message
-            
+
             suite_result["testcases"].append(case_result)
-        
+
         result["testsuites"].append(suite_result)
-    
+
     # Convert the result dictionary to JSON
     json_result = json.dumps(result, indent = indent)
     return json_result
