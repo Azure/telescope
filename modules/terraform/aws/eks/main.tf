23f84716dc8db4ee1ee8b2fc59d602ef30572aa0
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2024-10-11 15:07:02 -0400

    update vpc cni addon version to use latest.

--- modules/terraform/aws/eks/main.tf
+++ modules/terraform/aws/eks/main.tf
@@ -3,7 +3,7 @@ locals {
   eks_cluster_name   = "${var.eks_config.eks_name}-${var.run_id}"
   eks_node_group_map = { for node_group in var.eks_config.eks_managed_node_groups : node_group.name => node_group }
   karpenter_addons_map = {
-    for addon in [{ name = "vpc-cni", version = "v1.18.5-eksbuild.1", policy_arns = ["AmazonEKS_CNI_Policy"] }, { name = "kube-proxy" }, { name = "coredns" }] : addon.name =>
+    for addon in [{ name = "vpc-cni", policy_arns = ["AmazonEKS_CNI_Policy"] }, { name = "kube-proxy" }, { name = "coredns" }] : addon.name =>
     {
       name            = addon.name
       version         = lookup(addon, "version", null)
