55172051727831bc1228a49ccb7603821b50986d
Rafael Mendes Pereira <rafaelme@microsoft.com>
2024-11-01 16:22:22 +0000

    Revert "Set current_time as another input variable out from json_input"
    This reverts commit bfecac7cf7244dac0b8df5c13ed7c9feadbd4258.

--- modules/terraform/aws/variables.tf
+++ modules/terraform/aws/variables.tf
@@ -1,28 +1,24 @@
 variable "json_input" {
   description = "value of the json input"
   type = object({
-    run_id = string
-    region = string
+    run_id       = string
+    region       = string
+    current_time = string
   })
-}
-
-variable "current_time" {
-  description = "Current time as rfc3339 format (e.g.: '2024-10-17T18:30:42Z')"
-  type        = string
 
   validation {
-    condition     = can(formatdate("", var.current_time))
-    error_message = "The current_time value must be a valid rfc3339 format string (e.g.: '2024-10-17T18:30:42Z')"
+    condition     = can(formatdate("", var.json_input.current_time))
+    error_message = "The current_time value must be a valid rfc3339 format string"
   }
 
   validation {
-    condition     = timecmp(var.current_time, timeadd(plantimestamp(), "-1h")) > 0
-    error_message = "The current_time must not be older than 1h from now"
-  }
-
-  validation {
-    condition     = timecmp(var.current_time, timeadd(plantimestamp(), "+1h")) < 0
+    condition     = timecmp(var.json_input.current_time, timeadd(plantimestamp(), "-1h")) > 0
     error_message = "The current_time must not be younger than 1h from now"
+  }
+
+  validation {
+    condition     = timecmp(var.json_input.current_time, timeadd(plantimestamp(), "+1h")) < 0
+    error_message = "The current_time must not be older than 1h from now"
   }
 }
 
