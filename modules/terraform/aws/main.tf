2be3fef0ae30cb4e40626404b30d9949ed801615
Rafael Mendes Pereira <rafaelme@microsoft.com>
2024-11-20 18:47:19 +0000

    typo

--- modules/terraform/aws/main.tf
+++ modules/terraform/aws/main.tf
@@ -32,7 +32,7 @@ provider "aws" {
 check "deletion_due_time" {
   assert {
     condition     = timecmp(local.non_computed_tags.deletion_due_time, plantimestamp()) > 0
-    error_message = "Deletion due time is in the past: ${local.non_computed_tags.deletion_due_time}. This might result in the deletion of resources currenty in use. To resolve it, update the creation_tim (${local.creation_time}) to current time"
+    error_message = "Deletion due time is in the past: ${local.non_computed_tags.deletion_due_time}. This might result in the deletion of resources currenty in use. To resolve it, update the creation_time (${local.creation_time}) to current time"
   }
 }
 
