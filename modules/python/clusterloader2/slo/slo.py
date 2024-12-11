06dcf281ce2b51d17154c3db5efa3512a17bc6ec
karina-ranadive <karinaranadive@gmail.com>
2024-12-11 12:54:45 -0500

    added comma

--- modules/python/clusterloader2/slo/slo.py
+++ modules/python/clusterloader2/slo/slo.py
@@ -220,7 +220,7 @@ def main():
                                   help="Whether cilium is enabled. Must be either True or False")
     parser_configure.add_argument("service_test", type=eval, choices=[True, False], default=False,
                                   help="Whether service test is running. Must be either True or False")
-    parser_collect.add_argument("cnp_test", type=eval, choices=[True, False], nargs='?' default=False,
+    parser_collect.add_argument("cnp_test", type=eval, choices=[True, False], nargs='?', default=False,
                                   help="Whether service test is running. Must be either True or False")
     parser_collect.add_argument("ccnp_test", type=eval, choices=[True, False], nargs='?', default=False,
                                   help="Whether service test is running. Must be either True or False")
