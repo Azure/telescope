a325a16026f99671a4d0bc06812558a31b413637
jshr-w <shjayaraman@microsoft.com>
2024-12-16 12:35:49 -0800

    temp

--- modules/python/clusterloader2/slo/slo.py
+++ modules/python/clusterloader2/slo/slo.py
@@ -8,7 +8,7 @@ from utils import parse_xml_to_json, run_cl2_command, get_measurement
 from kubernetes_client import KubernetesClient
 
 DEFAULT_PODS_PER_NODE = 50
-LOAD_PODS_PER_NODE = 25 # Test Run, TODO: Switch to 100
+LOAD_PODS_PER_NODE = 50 # Test Run, TODO: Switch to 100
 
 DEFAULT_NODES_PER_NAMESPACE = 100
 CPU_REQUEST_LIMIT_MILLI = 1
