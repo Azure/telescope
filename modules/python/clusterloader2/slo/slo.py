956a261f766a28875f7d1ed4112ac6636b378893
karina-ranadive <karinaranadive@gmail.com>
2024-12-20 14:53:02 -0500

    added dualstack variable and adjusting user input parameters

--- modules/python/clusterloader2/slo/slo.py
+++ modules/python/clusterloader2/slo/slo.py
@@ -25,7 +25,7 @@ CPU_CAPACITY = {
 }
 # TODO: Remove aks once CL2 update provider name to be azure
 
-def calculate_config(cpu_per_node, node_count, pods_in_node, provider, service_test, cnp_test, ccnp_test, num_cnps, num_ccnps):
+def calculate_config(cpu_per_node, node_count, pods_in_node, provider, service_test, cnp_test, ccnp_test):
     throughput = 100
     nodes_per_namespace = min(node_count, DEFAULT_NODES_PER_NAMESPACE)
 
@@ -60,10 +60,11 @@ def configure_clusterloader2(
     ccnp_test,
     num_cnps,
     num_ccnps,
+    dualstack,
     override_file):
 
     steps = node_count // node_per_step
-    throughput, nodes_per_namespace, pods_per_node, cpu_request = calculate_config(cpu_per_node, node_per_step, pods_in_node, provider, service_test, cnp_test, ccnp_test, num_cnps, num_ccnps)
+    throughput, nodes_per_namespace, pods_per_node, cpu_request = calculate_config(cpu_per_node, node_per_step, pods_in_node, provider, service_test, cnp_test, ccnp_test)
 
     with open(override_file, 'w') as file:
         file.write(f"CL2_LOAD_TEST_THROUGHPUT: {throughput}\n")
@@ -95,10 +96,12 @@ def configure_clusterloader2(
         if cnp_test:
             file.write("CL2_CNP_TEST: true\n")
             file.write(f"CL2_CNPS_PER_NAMESPACE: {num_cnps}\n")
+            file.write(f"CL2_DUALSTACK: {dualstack}\n")
 
         if ccnp_test:
             file.write("CL2_CCNP_TEST: true\n")
             file.write(f"CL2_CCNPS: {num_ccnps}\n")
+            file.write(f"CL2_DUALSTACK: {dualstack}\n")
 
     with open(override_file, 'r') as file:
         print(f"Content of file {override_file}:\n{file.read()}")
@@ -138,6 +141,7 @@ def collect_clusterloader2(
     ccnp_test,
     num_cnps,
     num_ccnps,
+    dualstack,
     result_file,
     test_type="default_config",
 ):
@@ -151,7 +155,7 @@ def collect_clusterloader2(
     else:
         raise Exception(f"No testsuites found in the report! Raw data: {details}")
 
-    _, _, pods_per_node, _ = calculate_config(cpu_per_node, node_count, pods_in_node, provider, service_test, cnp_test, ccnp_test, num_cnps, num_ccnps)
+    _, _, pods_per_node, _ = calculate_config(cpu_per_node, node_count, pods_in_node, provider, service_test, cnp_test, ccnp_test)
     pod_count = node_count * pods_per_node
 
     # TODO: Expose optional parameter to include test details
@@ -224,11 +228,13 @@ def main():
     parser_configure.add_argument("service_test", type=eval, choices=[True, False], default=False,
                                   help="Whether service test is running. Must be either True or False")
     parser_configure.add_argument("cnp_test", type=eval, choices=[True, False], nargs='?', default=False,
-                                  help="Whether service test is running. Must be either True or False")
+                                  help="Whether cnp test is running. Must be either True or False")
     parser_configure.add_argument("ccnp_test", type=eval, choices=[True, False], nargs='?', default=False,
-                                  help="Whether service test is running. Must be either True or False")
+                                  help="Whether ccnp test is running. Must be either True or False")
     parser_configure.add_argument("num_cnps", type=int, nargs='?', default=0, help="Number of cnps")
     parser_configure.add_argument("num_ccnps", type=int, nargs='?', default=0, help="Number of ccnps")
+    parser_configure.add_argument("dualstack", type=eval, choices=[True, False], nargs='?', default=False,
+                                  help="Whether cluster is dualstack. Must be either True or False")
     parser_configure.add_argument("cl2_override_file", type=str, help="Path to the overrides of CL2 config file")
 
     # Sub-command for validate_clusterloader2
@@ -264,6 +270,8 @@ def main():
                                   help="Whether ccnp test is running. Must be either True or False")
     parser_collect.add_argument("num_cnps", type=int, nargs='?', default=0, help="Number of cnps")
     parser_collect.add_argument("num_ccnps", type=int, nargs='?', default=0, help="Number of ccnps")
+    parser_collect.add_argument("dualstack", type=eval, choices=[True, False], nargs='?', default=False,
+                                  help="Whether cluster is dualstack. Must be either True or False")
     parser_collect.add_argument("result_file", type=str, help="Path to the result file")
     parser_collect.add_argument("test_type", type=str, nargs='?', default="default-config",
                                 help="Description of test type")
@@ -273,7 +281,7 @@ def main():
     if args.command == "configure":
         configure_clusterloader2(args.cpu_per_node, args.node_count, args.node_per_step, args.max_pods,
                                  args.pods_in_node, args.repeats, args.operation_timeout, args.provider, args.cilium_enabled,
-                                 args.service_test, args.cnp_test, args.ccnp_test, args.num_cnps, args.num_ccnps, args.cl2_override_file)
+                                 args.service_test, args.cnp_test, args.ccnp_test, args.num_cnps, args.num_ccnps, args.dualstack, args.cl2_override_file)
     elif args.command == "validate":
         validate_clusterloader2(args.node_count, args.operation_timeout)
     elif args.command == "execute":
@@ -282,7 +290,7 @@ def main():
     elif args.command == "collect":
         collect_clusterloader2(args.cpu_per_node, args.node_count, args.max_pods, args.pods_in_node, args.repeats,
                                args.cl2_report_dir, args.cloud_info, args.run_id, args.run_url,
-                               args.service_test, args.cnp_test, args.ccnp_test, args.num_cnps, args.num_ccnps, args.result_file, args.test_type)
+                               args.service_test, args.cnp_test, args.ccnp_test, args.num_cnps, args.num_ccnps, args.dualstack, args.result_file, args.test_type)
 
 if __name__ == "__main__":
     main()
