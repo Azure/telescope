ddd4ad45ddbad2db4b12302fc99ad1305a23aa5d
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2025-01-14 16:38:19 +0000

    update

--- modules/terraform/azure/network/network-security-rule/main.tf
+++ modules/terraform/azure/network/network-security-rule/main.tf
@@ -1,4 +1,5 @@
 resource "azurerm_network_security_rule" "nsr" {
+  count                       = var.destination_port_range != null ? 1 : 0
   name                        = var.name
   priority                    = var.priority
   direction                   = var.direction
@@ -6,6 +7,21 @@ resource "azurerm_network_security_rule" "nsr" {
   protocol                    = var.protocol
   source_port_range           = var.source_port_range
   destination_port_range      = var.destination_port_range
+  destination_port_ranges     = var.destination_port_ranges
+  source_address_prefix       = var.source_address_prefix
+  destination_address_prefix  = var.destination_address_prefix
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
+}
+
+resource "azurerm_network_security_rule" "nsr-port-range" {
+  count                       = length(var.destination_port_ranges) > 0 ? 1 : 0
+  name                        = var.name
+  priority                    = var.priority
+  direction                   = var.direction
+  access                      = var.access
+  protocol                    = var.protocol
+  source_port_range           = var.source_port_range
   destination_port_ranges     = var.destination_port_ranges
   source_address_prefix       = var.source_address_prefix
   destination_address_prefix  = var.destination_address_prefix
