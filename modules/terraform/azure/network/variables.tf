d62a4045d04733cee428b1f728b1d0763cfc0bb8
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2025-01-14 16:31:23 +0000

    update

--- modules/terraform/azure/network/variables.tf
+++ modules/terraform/azure/network/variables.tf
@@ -50,7 +50,7 @@ variable "network_config" {
       access                     = string
       protocol                   = string
       source_port_range          = string
-      destination_port_range     = string
+      destination_port_range     = optional(string)
       destination_port_ranges    = optional(list(string), [])
       source_address_prefix      = string
       destination_address_prefix = string
--- modules/terraform/azure/variables.tf
+++ modules/terraform/azure/variables.tf
@@ -108,7 +108,7 @@ variable "network_config_list" {
       access                     = string
       protocol                   = string
       source_port_range          = string
-      destination_port_range     = string
+      destination_port_range     = optional(string)
       destination_port_ranges    = optional(list(string), [])
       source_address_prefix      = string
       destination_address_prefix = string
