79337b6c98ce528789cc1ccbeff0c3234948c905
alyssavu <alyssavu@microsoft.com>
2025-01-17 18:41:27 +0000

    adjust formula

--- modules/python/clusterloader2/cri/cri.py
+++ modules/python/clusterloader2/cri/cri.py
@@ -48,7 +48,7 @@ def override_config_clusterloader2(node_count, max_pods, repeats, operation_time
     pod_count = max_pods - DAEMONSETS_PER_NODE_MAP[provider]
     replica = pod_count * node_count
     cpu_request = cpu_value // pod_count
-    memory_request_in_Ki = math.ceil(memory_value // pod_count)
+    memory_request_in_Ki = math.ceil(memory_value // pod_count - 20)
     memory_request_in_K = int(memory_request_in_Ki // 1.024)
     print(f"CPU request for each pod: {cpu_request}m, memory request for each pod: {memory_request_in_K}K, total replica: {replica}")
 
