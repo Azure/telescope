524c438a7913d7d85100ebb1364c516febda7896
Sumanth Reddy CH <38838077+sumanthreddy29@users.noreply.github.com>
2025-01-27 15:23:48 -0500

    Update Nat-Gateway Module (#492)
    This pull request includes several changes to the Azure Network
    Terraform module to support multiple public IPs and subnets for NAT
    gateway associations. The most important changes include updating
    variable definitions, modifying resource configurations, and updating
    documentation.
    
    ### Variable Updates:
    * Updated `variables.tf` to replace `subnet_id` and
    `public_ip_address_id` with `subnet_names` and `public_ip_names`, and
    added new variables `nat_gateway_association` and `subnets_map` to
    support multiple associations.
    * Updated `network_config` and `network_config_list` variables to use
    lists of strings for `public_ip_names` and `subnet_names` instead of
    single string values.
    [[1]](diffhunk://#diff-2887bd794bdb916b262fac81e76d7a39c28c440aeb3fface88a98f8041613fa8L59-R60)
    [[2]](diffhunk://#diff-a70d527b3b015f3aa3103f024416bbf9c1e2828c82890a61a97316c23e5402efL117-R118)
    
    ### Resource Configuration:
    * Modified `main.tf` to handle multiple public IPs and subnets by
    updating the `azurerm_nat_gateway_public_ip_association` and
    `azurerm_subnet_nat_gateway_association` resources to use `count` and
    iterate over the lists of names.
    [[1]](diffhunk://#diff-72dcd3347bf4d3f128fcef665c145feec13fef9503bff62e37da8e02a4eaf23dR11-R20)
    [[2]](diffhunk://#diff-90c5a2cef35014d99f8d67f34be196c6c4ca724c0714a9649b6e81322af7cd93L106-R109)
    
    ### Documentation Updates:
    * Updated `README.md` files to reflect changes in variable names and
    types, ensuring the documentation aligns with the new configuration
    structure.
    [[1]](diffhunk://#diff-d3ba9935a46953d0978e91da0f98c2e775ecd1c5f85dc02174635a8f1f5671bdL22-R30)
    [[2]](diffhunk://#diff-d3ba9935a46953d0978e91da0f98c2e775ecd1c5f85dc02174635a8f1f5671bdL45-R46)
    
    ### Example Configuration:
    * Modified example configurations in `README.md` to use the new lists
    for `subnet_names` and `public_ip_names`.

--- modules/terraform/azure/network/nat-gateway/variables.tf
+++ modules/terraform/azure/network/nat-gateway/variables.tf
@@ -13,16 +13,25 @@ variable "nat_gateway_name" {
   type        = string
 }
 
-variable "subnet_id" {
-  description = "Value of the subnet id"
-  type        = string
-}
-
-variable "public_ip_address_id" {
-  description = "Value of the public ip address id"
-  type        = string
+variable "public_ips" {
+  description = "Map of public IP names to IDs"
+  type        = map(string)
 }
 
 variable "tags" {
   type = map(string)
+}
+
+variable "nat_gateway_association" {
+  description = "NAT Gateway association"
+  type = object({
+    nat_gateway_name = string
+    public_ip_names  = list(string)
+    subnet_names     = list(string)
+  })
+}
+
+variable "subnets_map" {
+  description = "Map of subnets"
+  type        = map(any)
 }
