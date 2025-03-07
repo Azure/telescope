def0aab5cf079243f4b65ea105bd23a53437a666
alyssavu <alyssavu@microsoft.com>
2025-03-07 21:19:50 +0000

    add user data for slo service churn test

---
+++ scenarios/perf-eval/slo-servicediscovery/scripts/user_data/client-userdata.sh
@@ -0,0 +1,22 @@
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="==MYBOUNDARY=="
+
+--==MYBOUNDARY==
+Content-Type: text/x-shellscript; charset="us-ascii"
+
+#!/bin/bash
+
+sudo tee -a /etc/eks/containerd/containerd-config.toml <<EOF
+
+[metrics]
+address = "0.0.0.0:10257"
+EOF
+
+# Restart containerd and kubelet to apply changes
+sudo systemctl restart containerd
+sudo systemctl restart kubelet
+
+cat /etc/eks/containerd/containerd-config.toml >> /var/log/user-data.log
+echo "Containerd metrics enabled on port 10257" >> /var/log/user-data.log
+
+--==MYBOUNDARY==--
