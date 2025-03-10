8e87f7d72aceb9c10bf06d0302811b13bc0c5e38
sanamsarath <sanamsarath@gmail.com>
2025-03-10 22:17:04 +0000

    Add NETWORK_POLICY_SOAK_MEASUREMENT_PREFIX to utils.py for measurement categorization

--- modules/python/clusterloader2/utils.py
+++ modules/python/clusterloader2/utils.py
@@ -13,6 +13,7 @@ POD_STARTUP_LATENCY_FILE_PREFIX_MEASUREMENT_MAP = {
 NETWORK_METRIC_PREFIXES = ["APIResponsivenessPrometheus", "InClusterNetworkLatency", "NetworkProgrammingLatency"]
 PROM_QUERY_PREFIX = "GenericPrometheusQuery"
 RESOURCE_USAGE_SUMMARY_PREFIX = "ResourceUsageSummary"
+NETWORK_POLICY_SOAK_MEASUREMENT_PREFIX = "NetworkPolicySoakMeasurement"
 
 def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, cl2_config_file="config.yaml", overrides=False, enable_prometheus=False, tear_down_prometheus=True,
                     enable_exec_service=False, scrape_kubelets=False):
@@ -65,7 +66,11 @@ def get_measurement(file_path):
     if file_name.startswith(RESOURCE_USAGE_SUMMARY_PREFIX):
         group_name = file_name.split("_")[1]
         return RESOURCE_USAGE_SUMMARY_PREFIX, group_name
+    if file_name.startswith(NETWORK_POLICY_SOAK_MEASUREMENT_PREFIX):
+        group_name = file_name.split("_")[1]
+        return NETWORK_POLICY_SOAK_MEASUREMENT_PREFIX, group_name
     return None, None
+
 
 def parse_xml_to_json(file_path, indent = 0):
     with open(file_path, 'r', encoding='utf-8') as file:
