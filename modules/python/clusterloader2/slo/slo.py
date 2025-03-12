d54510dff7a4f9ab4c4b5171eefdb1e82e4d640a
alyssavu <alyssavu@microsoft.com>
2025-03-12 21:32:30 +0000

    Only scrape containerd on AKS for now

--- modules/python/clusterloader2/slo/slo.py
+++ modules/python/clusterloader2/slo/slo.py
@@ -64,6 +64,8 @@ def configure_clusterloader2(
     steps = node_count // node_per_step
     throughput, nodes_per_namespace, pods_per_node, cpu_request = calculate_config(cpu_per_node, node_per_step, max_pods, provider, service_test, cnp_test, ccnp_test)
 
+    scraped = provider == "aks" and scrape_containerd
+
     with open(override_file, 'w', encoding='utf-8') as file:
         file.write(f"CL2_NODES: {node_count}\n")
         file.write(f"CL2_LOAD_TEST_THROUGHPUT: {throughput}\n")
@@ -81,7 +83,7 @@ def configure_clusterloader2(
         file.write("CL2_PROMETHEUS_CPU_SCALE_FACTOR: 30.0\n")
         file.write("CL2_PROMETHEUS_NODE_SELECTOR: \"prometheus: \\\"true\\\"\"\n")
         file.write("CL2_POD_STARTUP_LATENCY_THRESHOLD: 3m\n")
-        file.write(f"CL2_SCRAPE_CONTAINERD: {str(scrape_containerd).lower()}\n")
+        file.write(f"CL2_SCRAPE_CONTAINERD: {str(scraped).lower()}\n")
         file.write("CONTAINERD_SCRAPE_INTERVAL: 5m\n")
 
         if cilium_enabled:
@@ -136,9 +138,10 @@ def execute_clusterloader2(
     provider,
     scrape_containerd
 ):
+    scraped = provider == "aks" and scrape_containerd
     run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider,
                     cl2_config_file=cl2_config_file, overrides=True, enable_prometheus=True,
-                    scrape_containerd=scrape_containerd, tear_down_prometheus=False)
+                    scrape_containerd=scraped, tear_down_prometheus=False)
 
 def collect_clusterloader2(
     cpu_per_node,
