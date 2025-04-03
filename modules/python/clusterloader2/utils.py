6528f7b1adbe3a66c7508eeca65a1d64cf997861
Beilei Huang <beileihuang@microsoft.com>
2025-04-04 08:54:53 +1300

    restore function in utils

--- modules/python/clusterloader2/utils.py
+++ modules/python/clusterloader2/utils.py
@@ -5,6 +5,15 @@ import docker
 
 from docker_client import DockerClient
 
+POD_STARTUP_LATENCY_FILE_PREFIX_MEASUREMENT_MAP = {
+    "PodStartupLatency_PodStartupLatency_": "PodStartupLatency_PodStartupLatency",
+    "StatefulPodStartupLatency_PodStartupLatency_": "StatefulPodStartupLatency_PodStartupLatency",
+    "StatelessPodStartupLatency_PodStartupLatency_": "StatelessPodStartupLatency_PodStartupLatency",
+}
+NETWORK_METRIC_PREFIXES = ["APIResponsivenessPrometheus", "InClusterNetworkLatency", "NetworkProgrammingLatency"]
+PROM_QUERY_PREFIX = "GenericPrometheusQuery"
+RESOURCE_USAGE_SUMMARY_PREFIX = "ResourceUsageSummary"
+
 def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, cl2_config_file="config.yaml", overrides=False, enable_prometheus=False, tear_down_prometheus=True,
                     enable_exec_service=False, scrape_kubelets=False, scrape_containerd=False):
     docker_client = DockerClient()
@@ -97,3 +106,22 @@ def parse_xml_to_json(file_path, indent = 0):
     # Convert the result dictionary to JSON
     json_result = json.dumps(result, indent = indent)
     return json_result
+
+def get_measurement(file_path):
+    file_name = os.path.basename(file_path)
+    for file_prefix, measurement in POD_STARTUP_LATENCY_FILE_PREFIX_MEASUREMENT_MAP.items():
+        if file_name.startswith(file_prefix):
+            group_name = file_name.split("_")[2]
+            return measurement, group_name
+    for file_prefix in NETWORK_METRIC_PREFIXES:
+        if file_name.startswith(file_prefix):
+            group_name = file_name.split("_")[1]
+            return file_prefix, group_name
+    if file_name.startswith(PROM_QUERY_PREFIX):
+        group_name = file_name.split("_")[1]
+        measurement_name = file_name.split("_")[0][len(PROM_QUERY_PREFIX)+1:]
+        return measurement_name, group_name
+    if file_name.startswith(RESOURCE_USAGE_SUMMARY_PREFIX):
+        group_name = file_name.split("_")[1]
+        return RESOURCE_USAGE_SUMMARY_PREFIX, group_name
+    return None, None
