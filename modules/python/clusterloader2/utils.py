7f18f098a461ebd8e2df91f67491590685a8482e
Alex Castilio dos Santos <alexsantos@microsoft.com>
2025-05-01 15:28:40 +0100

    enable scrape kube-state-metrics
    Signed-off-by: Alex Castilio dos Santos <alexsantos@microsoft.com>

--- modules/python/clusterloader2/utils.py
+++ modules/python/clusterloader2/utils.py
@@ -31,6 +31,7 @@ def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provi
 --testconfig /root/perf-tests/clusterloader2/config/{cl2_config_file}
 --report-dir /root/perf-tests/clusterloader2/results
 --tear-down-prometheus-server={tear_down_prometheus}
+--prometheus-scrape-kube-state-metrics=True
 --prometheus-scrape-metrics-server=True"""
 
     if scrape_containerd:
