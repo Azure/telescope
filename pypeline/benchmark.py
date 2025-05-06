72ae9c79fa4fc34197c3c348acf00921394713ac
vittoriasalim <vittoria908salim@gmail.com>
2025-05-06 14:54:53 +1000

    reformat

--- pypeline/benchmark.py
+++ pypeline/benchmark.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from resource.setup import Setup
 
 import yaml
-
 from components import Cloud, Engine, Resource
 from pipeline import Job, Pipeline, Stage, customize_yaml
 from terraform.terraform import Terraform
