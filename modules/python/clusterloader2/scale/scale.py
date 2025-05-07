b6353f367902a3f8a71459b6de53e6afb9bce046
Alex Castilio dos Santos <alexsantos@microsoft.com>
2025-05-07 10:35:49 +0100

    change scrape_kubelets to True
    Signed-off-by: Alex Castilio dos Santos <alexsantos@microsoft.com>

--- modules/python/clusterloader2/scale/scale.py
+++ modules/python/clusterloader2/scale/scale.py
@@ -161,7 +161,7 @@ def execute_clusterloader2(
 ):
     run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider,
                     cl2_config_file=cl2_config_file, overrides=True, enable_prometheus=True,
-                    scrape_containerd=scrape_containerd, tear_down_prometheus=False, scrape_kubelets=False)
+                    scrape_containerd=scrape_containerd, tear_down_prometheus=False, scrape_kubelets=True)
 
 def collect_clusterloader2(
     cpu_per_node,
