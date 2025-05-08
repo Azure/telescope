7427f9b6179bf77a85a7e03a1f9d20fc19046061
Vittoria <81071513+vittoriasalim@users.noreply.github.com>
2025-05-08 13:16:50 +1000

    refactor setup, move logic outside of script (#632)
    - Added validate_owner_info at setup.py
    - Refactor setup function , remove environment variables and move logic
    outside of script

--- pypeline/resource/setup.py
+++ pypeline/resource/setup.py
@@ -1,80 +1,94 @@
 from dataclasses import dataclass
 from textwrap import dedent
 
-from benchmark import Resource
 from pipeline import Script, Step
+from benchmark import Resource
 
-set_run_id = lambda run_id: Script(
-    display_name="Set run id",
-    script=dedent(
-        """
-        if [ -n "$RUN_ID" ]; then
-        run_id=$RUN_ID
-        else
-        run_id=$(Build.BuildId)-$(System.JobId)
-        fi
-        echo "Run ID: $run_id"
-        echo "##vso[task.setvariable variable=RUN_ID]$run_id"
-        """.strip(
-            "\n"
-        )
-    ),
-    env={"RUN_ID": run_id},
-)
 
-set_run_url_and_code_url = Script(
-    display_name="Set Run URL & Code URL",
-    script=dedent(
-        """
-        run_url="$(System.TeamFoundationCollectionUri)$(System.TeamProject)/_build/results?buildId=$(Build.BuildId)&view=logs&j=$(System.JobId)"
-        echo "Run URL: $run_url"
-        echo "##vso[task.setvariable variable=RUN_URL]$run_url"
+def set_run_id(run_id: str) -> Script:
+    if not run_id:
+        run_id = "$(Build.BuildId)-$(System.JobId)"
 
-        code_url="$(Build.Repository.Uri)/commit/$(Build.SourceVersion)"
-        echo "Code URL: $code_url"
-        echo "##vso[task.setvariable variable=CODE_URL]$code_url"
-        """.strip(
-            "\n"
-        )
-    ),
-)
+    return Script(
+        display_name="Set run id",
+        script=dedent(
+            f"""
+            echo "Run ID: {run_id}"
+            echo "##vso[task.setvariable variable=RUN_ID]{run_id}"
+            """
+        ).strip(),
+    )
 
-set_test_results_directory = Script(
-    display_name="Set Test Results Directory",
-    script=dedent(
-        """
-        test_results_directory=$(Pipeline.Workspace)/s/$(RUN_ID)
-        mkdir -p $test_results_directory
-        echo "Test Results directory: $test_results_directory"
-        echo "##vso[task.setvariable variable=TEST_RESULTS_DIR]$test_results_directory"
 
-        test_results_file=$test_results_directory/results.json
+def set_run_url_and_code_url() -> Script:
 
-        echo "Test Results file: $test_results_file"
-        echo "##vso[task.setvariable variable=TEST_RESULTS_FILE]$test_results_file"
-        """.strip(
-            "\n"
-        )
-    ),
-)
+    return Script(
+        display_name="Set Run URL & Code URL",
+        script=dedent(
+            """
+            run_url="$(System.TeamFoundationCollectionUri)$(System.TeamProject)/_build/results?buildId=$(Build.BuildId)&view=logs&j=$(System.JobId)"
+            echo "Run URL: $run_url"
+            echo "##vso[task.setvariable variable=RUN_URL]$run_url"
 
-set_script_module_directory = lambda test_modules_dir: Script(
-    display_name="Set Script Module Directory",
-    script=dedent(
-        """
-        if [ -n "${TEST_MODULES_DIR}" ]; then
-            test_modules_directory=$(Pipeline.Workspace)/s/${TEST_MODULES_DIR}
-        else
-            test_modules_directory=$(Pipeline.Workspace)/s/modules/bash
-        fi
-        echo "Script modules directory: $test_modules_directory"
-        echo "##vso[task.setvariable variable=TEST_MODULES_DIR]$test_modules_directory"
-        """.strip(
-            "\n"
-        )
-    ),
-    env={"TEST_MODULES_DIR": test_modules_dir},
-)
+            code_url="$(Build.Repository.Uri)/commit/$(Build.SourceVersion)"
+            echo "Code URL: $code_url"
+            echo "##vso[task.setvariable variable=CODE_URL]$code_url"
+            """
+        ).strip(),
+    )
+
+
+def set_test_results_directory() -> Script:
+
+    return Script(
+        display_name="Set Test Results Directory",
+        script=dedent(
+            """
+            test_results_directory=$(Pipeline.Workspace)/s/$(RUN_ID)
+            mkdir -p $test_results_directory
+            echo "Test Results directory: $test_results_directory"
+
+            test_results_file=$test_results_directory/results.json
+
+            echo "Test Results file: $test_results_file"
+            echo "##vso[task.setvariable variable=TEST_RESULTS_FILE]$test_results_file"
+            """
+        ).strip(),
+    )
+
+
+def set_script_module_directory(test_modules_dir: str) -> Script:
+    if not test_modules_dir:
+        test_modules_dir = "modules/bash"
+
+    script_modules_directory = f"$(Pipeline.Workspace)/s/{test_modules_dir}"
+
+    return Script(
+        display_name="Set Script Module Directory",
+        script=dedent(
+            f"""
+            echo "Script modules directory: {script_modules_directory}"
+            echo "##vso[task.setvariable variable=TEST_MODULES_DIR]{script_modules_directory}"
+            """
+        ).strip(),
+    )
+
+
+def validate_owner_info() -> Script:
+
+    return Script(
+        display_name="Validate OWNER info",
+        script=dedent(
+            """
+            # Check if OWNER has been set to any string value other than an empty string
+            if [ -z "$OWNER" ]; then
+                echo "##vso[task.logissue type=error;] OWNER is not set. Please set OWNER to a valid value ('aks', 'compute', 'networking', 'storage') in the pipeline."
+                exit 1
+            fi
+            """
+        ).strip(),
+        condition="eq(variables['SKIP_RESOURCE_MANAGEMENT'], 'true')",
+    )
 
 
 @dataclass
@@ -85,13 +99,13 @@ class Setup(Resource):
     def setup(self) -> list[Step]:
         return [
             set_run_id(self.run_id),
-            set_run_url_and_code_url,
-            set_test_results_directory,
+            set_run_url_and_code_url(),
+            set_test_results_directory(),
             set_script_module_directory(self.test_module_dir),
         ]
 
     def validate(self) -> list[Step]:
-        return []
+        return [validate_owner_info()]
 
     def tear_down(self) -> list[Step]:
         return []
