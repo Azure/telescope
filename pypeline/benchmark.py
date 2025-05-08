54ef8897ef759c9e1884a793bb397786114a00f2
Vittoria <81071513+vittoriasalim@users.noreply.github.com>
2025-05-08 13:17:30 +1000

    Fix encoding for writing yaml (#633)
    - specify encoding for writing yaml file 
    - fix spelling error for resources

--- pypeline/benchmark.py
+++ pypeline/benchmark.py
@@ -38,7 +38,7 @@ class Layout:
     display_name: str
     setup: Resource
     cloud: Cloud
-    resouces: list[Resource]
+    resources: list[Resource]
     engine: Engine
 
     def get_jobs(self) -> list[Job]:
@@ -47,13 +47,13 @@ class Layout:
             display_name="Setup resources",
             steps=self.setup.setup()
             + self.cloud.login()
-            + [step for r in self.resouces for step in r.setup()]
+            + [step for r in self.resources for step in r.setup()]
             + self.engine.setup(),
         )
         validate = Job(
             job="validate",
             display_name="Validate resources",
-            steps=[step for r in self.resouces for step in r.validate()]
+            steps=[step for r in self.resources for step in r.validate()]
             + self.engine.validate(),
             depends_on=[setup.job],
         )
@@ -68,7 +68,7 @@ class Layout:
             display_name="Tear down resources",
             # Tears down in reverse order of setup.
             steps=self.engine.tear_down()
-            + [step for r in self.resouces[::-1] for step in r.tear_down()],
+            + [step for r in self.resources[::-1] for step in r.tear_down()],
             depends_on=[run.job],
         )
         return [
@@ -117,5 +117,5 @@ class Benchmark:
         self.prepare_directory(yaml_file_path)
 
         customize_yaml()
-        with open(yaml_file_path, "w") as file:
+        with open(yaml_file_path, "w", encoding="utf-8") as file:
             yaml.dump(p, file, sort_keys=False)
