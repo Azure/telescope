591386af8f2cdff1bbdbe940612da21c949f9029
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2025-05-09 17:15:10 +0000

    update

--- modules/python/pipelines/pipelines.py
+++ modules/python/pipelines/pipelines.py
@@ -46,11 +46,10 @@ def disable_pipeline(org, project, pipeline_def, headers):
     pipeline_def["queueStatus"] = "disabled"
     pipeline_def["comment"] = "Disabled by script to prevent scheduling on non-main branches."
     definition_id = pipeline_def["id"]
-    if definition_id == 72:
-        url = f"https://dev.azure.com/{org}/{project}/_apis/build/definitions/{definition_id}?api-version=7.1-preview.7"
-        res = requests.put(url, json=pipeline_def, headers=headers, timeout=10)
-        res.raise_for_status()
-        logger.info(f"✅ Disabled pipeline: {pipeline_def['name']} under {pipeline_def['path']}")
+    url = f"https://dev.azure.com/{org}/{project}/_apis/build/definitions/{definition_id}?api-version=7.1-preview.7"
+    res = requests.put(url, json=pipeline_def, headers=headers, timeout=10)
+    res.raise_for_status()
+    logger.info(f"✅ Disabled pipeline: {pipeline_def['name']} under {pipeline_def['path']}")
 
 def main():
     parser = argparse.ArgumentParser(description="Validate and disable ADO pipelines not scheduled on 'main' branch.")
