68951dd8107c69cbae2b14884ffafaf9f6a65290
Anton Pegushin <anpegush@microsoft.com>
2025-05-12 14:15:12 -0700

    Addressing PR comments.

--- modules/python/kbench/validate.py
+++ modules/python/kbench/validate.py
@@ -6,18 +6,7 @@ from clients.kubernetes_client import KubernetesClient
 
 def validate(node_count, operation_timeout_in_minutes=10):
     kube_client = KubernetesClient()
-    ready_node_count = 0
-    timeout = time.time() + (operation_timeout_in_minutes * 60)
-    while time.time() < timeout:
-        ready_nodes = kube_client.get_ready_nodes()
-        ready_node_count = len(ready_nodes)
-        print(f"Currently {ready_node_count} nodes are ready.")
-        if ready_node_count == node_count:
-            break
-        print(f"Waiting for {node_count} nodes to be ready.")
-        time.sleep(10)
-    if ready_node_count != node_count:
-        raise Exception(f"Only {ready_node_count} nodes are ready, expected {node_count} nodes!")
+    kube_client.wait_for_nodes_ready(node_count, operation_timeout_in_minutes)
 
 def main():
     parser = argparse.ArgumentParser(description="Validate k8s setup for a kbench CRI run.")
