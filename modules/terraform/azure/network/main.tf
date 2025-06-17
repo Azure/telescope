c6f5c63632cc96c7b0ddd2eb18600122e33ab920
alyssavu <alyssavu@microsoft.com>
2025-06-17 22:03:38 +0000

    clean up

--- modules/terraform/azure/network/main.tf
+++ modules/terraform/azure/network/main.tf
@@ -3,7 +3,6 @@ locals {
   nat_gateway_associations_map = var.network_config.nat_gateway_associations == null ? {} : { for nat in var.network_config.nat_gateway_associations : nat.nat_gateway_name => nat }
   vnet_name                    = var.network_config.vnet_name
   input_subnet_map             = { for subnet in var.network_config.subnet : subnet.name => subnet }
-  # subnets_map                  = { for subnet in azurerm_subnet.subnets : subnet.name => subnet }
   subnets_map = {
     for subnet_id in azurerm_virtual_network.vnet.subnet.*.id :
     split("/", subnet_id)[10] => subnet_id
@@ -21,15 +20,6 @@ locals {
   ])
   nic_association_map = { for nic in local.expanded_nic_association_map : nic.nic_name => nic }
   tags                = merge(var.tags, { "role" = var.network_config.role })
-
-  # NRMS-NSG-Rule-101_1.3 : Allow 443 from Virtual Network
-  # NRMS-NSG-Rule-103_1.4 : Allow all traffic from corp net
-  # NRMS-NSG-Rule-104_1.3 : Allow all traffic from CorpNetSaw
-  # NRMS-NSG-Rule-105_1.3 : Block Port "1433","1434","3306","4333","5432","6379","7000","7001","7199","9042","9160","9300","16379","26379","27017" from Internet
-  # NRMS-NSG-Rule-106_1.3 : Block Port "22","3389" from Internet
-  # NRMS-NSG-Rule-107_1.3 : Block Port "23","135","445","5985","5986" from Internet
-  # NRMS-NSG-Rule-108_1.3 : Block Port "13","17","19","53","69","111","123","512","514","593","873","1900","5353","11211" from Internet
-  # NRMS-NSG-Rule-109_1.3 : Block Port "119","137","138","139","161","162","389","636","2049","2301","2381","3268","5800","5900" from Internet
 }
 
 resource "azurerm_network_security_group" "nsg" {
@@ -59,37 +49,8 @@ resource "azurerm_virtual_network" "vnet" {
   depends_on = [
     azurerm_network_security_group.nsg,
     module.nsr
-    # module.nrms_security_rules
   ]
 }
-
-# resource "azurerm_subnet" "subnets" {
-#   for_each = local.input_subnet_map
-
-#   name                                          = each.value.name
-#   resource_group_name                           = var.resource_group_name
-#   virtual_network_name                          = azurerm_virtual_network.vnet.name
-#   address_prefixes                              = [each.value.address_prefix]
-#   service_endpoints                             = each.value.service_endpoints != null ? each.value.service_endpoints : []
-#   private_link_service_network_policies_enabled = each.value.pls_network_policies_enabled != null ? each.value.pls_network_policies_enabled : true
-#   dynamic "delegation" {
-#     for_each = each.value.delegations != null ? each.value.delegations : []
-#     content {
-#       name = delegation.value.name
-#       service_delegation {
-#         name    = delegation.value.service_delegation_name
-#         actions = delegation.value.service_delegation_actions
-#       }
-#     }
-#   }
-# }
-
-# resource "azurerm_subnet_network_security_group_association" "subnet-nsg-associations" {
-#   for_each = local.network_security_group_name != "" ? local.subnets_map : {}
-
-#   subnet_id                 = each.value.id
-#   network_security_group_id = azurerm_network_security_group.nsg[0].id
-# }
 
 resource "azurerm_network_interface" "nic" {
   for_each = local.nic_association_map
@@ -124,13 +85,6 @@ module "nsr" {
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.nsg[0].name
 }
-
-# module "nrms_security_rules" {
-#   source = "./nrms-security-rules"
-
-#   resource_group_name         = var.resource_group_name
-#   network_security_group_name = azurerm_network_security_group.nsg[0].name
-# }
 
 module "nat_gateway" {
   source   = "./nat-gateway"
