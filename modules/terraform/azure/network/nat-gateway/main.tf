3c212f2b3d3b16ea1a02e77d38fc8d71d56b7e5e
Sumanth Reddy Chinna Pullaiah <schinnapulla@microsoft.com>
2025-06-18 14:29:18 +0000

    update Nat Gateway Subnet

--- modules/terraform/azure/network/nat-gateway/main.tf
+++ modules/terraform/azure/network/nat-gateway/main.tf
@@ -17,6 +17,6 @@ resource "azurerm_nat_gateway_public_ip_association" "nat_gateway_ip_association
 resource "azurerm_subnet_nat_gateway_association" "nat_gateway_subnet_association" {
   count = length(var.nat_gateway_association.subnet_names)
 
-  subnet_id      = var.subnets_map[var.nat_gateway_association.subnet_names[count.index]].id
+  subnet_id      = var.subnets_map[var.nat_gateway_association.subnet_names[count.index]]
   nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
 }
