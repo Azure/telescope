3f5dff6d286af6ebf752ec4fe01aca36dccf4754
vittoriasalim <vittoria908salim@gmail.com>
2025-06-30 16:04:55 +1000

    remove pod from kwok

--- modules/python/kwok/kwok.py
+++ modules/python/kwok/kwok.py
@@ -1,6 +1,5 @@
 import argparse
 import subprocess
-import sys
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
@@ -31,7 +30,6 @@ class KWOK(ABC):
         stage_fast_yaml_url = f"https://github.com/{self.kwok_repo}/releases/download/{kwok_release}/stage-fast.yaml"
         subprocess.run(["kubectl", "apply", "-f", kwok_yaml_url], check=True)
         subprocess.run(["kubectl", "apply", "-f", stage_fast_yaml_url], check=True)
-        
         if enable_metrics:
             metrics_usage_url = f"https://github.com/{self.kwok_repo}/releases/download/{kwok_release}/metrics-usage.yaml"
             subprocess.run(["kubectl", "apply", "-f", metrics_usage_url], check=True)
