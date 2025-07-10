c3fc2537d8f39d6ebe07bb96a6956c9f464c33b0
vittoriasalim <vittoria908salim@gmail.com>
2025-07-10 16:20:44 +1000

    fix literal string

--- pypeline/terraform/terraform.py
+++ pypeline/terraform/terraform.py
@@ -111,7 +111,7 @@ def set_input(
         region_input_variables = cloud.generate_input_variables(region, input_variables)
         if region not in regional_config:
             regional_config[region] = {}  # Ensure key exists
-        regional_config[region]["TERRAFORM_INPUT_VARIABLES"] = json.dumps(region_input_variables)
+        regional_config[region]["TERRAFORM_INPUT_VARIABLES"] = region_input_variables
 
     # Convert regional configuration to JSON
     regional_config_str = json.dumps(regional_config)
