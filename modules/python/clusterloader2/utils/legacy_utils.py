08b46532ca5aae950f1a9770cac5dfe91d49207b
Cuong Nguyen <nguyencuong@microsoft.com>
2025-09-03 17:52:33 +0100

    Use legacy utils to fit the test case

--- modules/python/clusterloader2/utils/legacy_utils.py
+++ modules/python/clusterloader2/utils/legacy_utils.py
@@ -6,6 +6,7 @@ these module-level names continue to work.
 """
 from .cl2_command import Cl2Command
 from .xml_to_json_parser import Xml2JsonParser
+from .cl2_report_parser import Cl2ReportProcessor
 
 
 def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, **kwargs):
@@ -28,3 +29,9 @@ def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provi
 def parse_xml_to_json(filepath, indent=0):
     """Legacy wrapper for Xml2JsonParser.parse()."""
     return Xml2JsonParser(filepath, indent=indent).parse()
+
+
+def process_cl2_report(cl2_report_dir: str, template: dict):
+    """Legacy wrapper for Cl2ReportProcessor.process()."""
+    return Cl2ReportProcessor(cl2_report_dir=cl2_report_dir, 
+                              template=template).process()
