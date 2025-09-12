50106102cbb8ff488bec8632df3dc643fcc5a3a9
Cuong Nguyen <nguyenluan3071@gmail.com>
2025-09-12 13:28:46 +0000

    Add constants

--- modules/python/utils/constants.py
+++ modules/python/utils/constants.py
@@ -22,3 +22,19 @@ class AzureSKUFamily:
     VM_SIZE_TO_SKU_FAMILY = {
         "Standard_ND96asr_v4": "ndv4"
     }
+
+@dataclass(frozen=True)
+class MeasurementPrefixConstants:
+    POD_STARTUP_LATENCY_FILE_PREFIX_MEASUREMENT_MAP = {
+        "PodStartupLatency_PodStartupLatency_": "PodStartupLatency_PodStartupLatency",
+        "StatefulPodStartupLatency_PodStartupLatency_": "StatefulPodStartupLatency_PodStartupLatency",
+        "StatelessPodStartupLatency_PodStartupLatency_": "StatelessPodStartupLatency_PodStartupLatency",
+    }
+    NETWORK_METRIC_PREFIXES = ["APIResponsivenessPrometheus",
+                            "InClusterNetworkLatency", "NetworkProgrammingLatency"]
+    PROM_QUERY_PREFIX = "GenericPrometheusQuery"
+    RESOURCE_USAGE_SUMMARY_PREFIX = "ResourceUsageSummary"
+    NETWORK_POLICY_SOAK_MEASUREMENT_PREFIX = "NetworkPolicySoakMeasurement"
+    JOB_LIFECYCLE_LATENCY_PREFIX = "JobLifecycleLatency"
+    SCHEDULING_THROUGHPUT_PROMETHEUS_PREFIX = "SchedulingThroughputPrometheus"
+    SCHEDULING_THROUGHPUT_PREFIX = "SchedulingThroughput"
