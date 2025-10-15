396e476e812172fa98137afdb829e89da8e1fbd0
Isaac <jojosep@microsoft.com>
2025-10-15 21:35:06 +0000

    refactor: runCustomerScripts

--- swiftv2kubeobjects/shared-config.sh
+++ swiftv2kubeobjects/shared-config.sh
@@ -3,21 +3,21 @@
 # This file should be sourced by both runCustomerSetup.sh and createclusterforping.sh
 
 # Common subscription and location settings
-CUST_SUB=${SUBSCRIPTION:-"9b8218f9-902a-4d20-a65c-e98acec5362f"}
-LOCATION=${LOCATION:-"uksouth"}
+export CUST_SUB=${SUBSCRIPTION:-"9b8218f9-902a-4d20-a65c-e98acec5362f"}
+export LOCATION=${LOCATION:-"eastus2euap"}
 
 # Customer VNet configuration
-CUST_VNET_NAME="custvnet"
-CUST_SCALE_DEL_SUBNET="scaledel"
-CUST_AKS_NODE_SUBNET="aksnodes"
-CUST_AKS_POD_SUBNET="akspods"
+export CUST_VNET_NAME="custvnet"
+export CUST_SCALE_DEL_SUBNET="scaledel"
+export CUST_AKS_NODE_SUBNET="aksnodes"
+export CUST_AKS_POD_SUBNET="akspods"
 
 # Resource group naming - use dynamic naming based on location
-CUST_RG="sv2-perf-cust-$LOCATION"
+export CUST_RG="sv2-perf-cust-$LOCATION"
 
 # Shared identity names
-SHARED_KUBELET_IDENTITY_NAME="sharedKubeletIdentity"
-SHARED_CONTROL_PLANE_IDENTITY_NAME="sharedControlPlaneIdentity"
+export SHARED_KUBELET_IDENTITY_NAME="sharedKubeletIdentity"
+export SHARED_CONTROL_PLANE_IDENTITY_NAME="sharedControlPlaneIdentity"
 
 # Export all variables so they're available to child processes
 export CUST_SUB LOCATION CUST_VNET_NAME CUST_SCALE_DEL_SUBNET CUST_AKS_NODE_SUBNET CUST_AKS_POD_SUBNET
