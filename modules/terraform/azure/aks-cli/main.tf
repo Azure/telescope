6b01a2d6e329320da751b73631b2de6761a79e76
Xinwei Liu <nadialiuwei@gmail.com>
2025-11-25 18:01:11 +1100

    Delete Key Vault Crypto User role for key

--- modules/terraform/azure/aks-cli/main.tf
+++ modules/terraform/azure/aks-cli/main.tf
@@ -185,7 +185,6 @@ resource "terraform_data" "aks_cli" {
     terraform_data.enable_aks_cli_preview_extension,
     azurerm_role_assignment.network_contributor,
     azurerm_role_assignment.network_contributor_api_server_subnet,
-    azurerm_role_assignment.aks_key_service_crypto_user,
     azurerm_role_assignment.aks_key_service_encryption_user,
     azurerm_role_assignment.aks_kv_service_encryption_user
   ]
@@ -236,12 +235,6 @@ resource "terraform_data" "aks_nodepool_cli" {
 }
 
 # Grant Key Vault Crypto Service Encryption User role for KMS encryption
-resource "azurerm_role_assignment" "aks_key_service_crypto_user" {
-  count                = var.key_management_service == null ? 0 : 1
-  scope                = var.key_management_service.key_vault_key_resource_id
-  role_definition_name = "Key Vault Crypto User"
-  principal_id         = azurerm_user_assigned_identity.userassignedidentity[0].principal_id
-}
 resource "azurerm_role_assignment" "aks_key_service_encryption_user" {
   count                = var.key_management_service == null ? 0 : 1
   scope                = var.key_management_service.key_vault_key_resource_id
