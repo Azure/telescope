d6cbc8e8f5e3cb4f540ca1868275ec2a0dc728d1
Xinwei Liu <nadialiuwei@gmail.com>
2025-11-25 11:59:31 +1100

    Disable recover_soft_deleted_key_vaults

--- modules/terraform/azure/main.tf
+++ modules/terraform/azure/main.tf
@@ -61,7 +61,7 @@ provider "azurerm" {
   features {
     key_vault {
       purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
+      recover_soft_deleted_key_vaults = false
     }
   }
 }
