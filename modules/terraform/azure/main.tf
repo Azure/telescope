c9bbe61ea0eff4f53d924d39c0721f8523a699f5
Xinwei Liu <nadialiuwei@gmail.com>
2025-12-03 17:12:53 +1100

    Change the default value to boolean

--- modules/terraform/azure/main.tf
+++ modules/terraform/azure/main.tf
@@ -10,7 +10,7 @@ locals {
   aks_custom_headers                = lookup(var.json_input, "aks_custom_headers", [])
   k8s_machine_type                  = lookup(var.json_input, "k8s_machine_type", null)
   k8s_os_disk_type                  = lookup(var.json_input, "k8s_os_disk_type", null)
-  aks_aad_enabled                   = lookup(var.json_input, "aks_aad_enabled", "false")
+  aks_aad_enabled                   = lookup(var.json_input, "aks_aad_enabled", false)
   enable_apiserver_vnet_integration = lookup(var.json_input, "enable_apiserver_vnet_integration", false)
 
   tags = {
