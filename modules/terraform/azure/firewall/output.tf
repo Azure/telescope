352888a01592007892f7603c6ab062ab3e342faa
vittoria salim <vslaim@microsoft.com>
2025-12-05 22:43:23 +1100

    fix firewall output

--- modules/terraform/azure/firewall/output.tf
+++ modules/terraform/azure/firewall/output.tf
@@ -1,7 +1,8 @@
 output "firewall_private_ips" {
   description = "Map of firewall names to their private IP addresses"
+  
   value = {
-    for fw_name, fw in azurerm_firewall.firewall : fw_name => fw.ip_configuration[0].private_ip_address
+    for fw in azurerm_firewall.firewall : fw.name => fw.ip_configuration[0].private_ip_address
   }
   
   depends_on = [
