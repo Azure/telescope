484a47a77091fae2bc4d4be35b265fa67394f175
vittoria salim <vslaim@microsoft.com>
2025-12-05 23:37:06 +1100

    make output no dependency

--- modules/terraform/azure/firewall/output.tf
+++ modules/terraform/azure/firewall/output.tf
@@ -3,19 +3,4 @@ output "firewall_private_ips" {
   value = {
     for fw_name, fw in azurerm_firewall.firewall : fw.name => fw.ip_configuration[0].private_ip_address
   }
-  
-  depends_on = [
-    azurerm_firewall_nat_rule_collection.nat_rules,
-    azurerm_firewall_network_rule_collection.network_rules,
-    azurerm_firewall_application_rule_collection.application_rules
-  ]
-
-  # Ensure all firewalls have assigned private IPs
-  precondition {
-    condition = alltrue([
-      for fw_name, fw in azurerm_firewall.firewall : 
-      fw.ip_configuration[0].private_ip_address != null && fw.ip_configuration[0].private_ip_address != ""
-    ])
-    error_message = "One or more firewalls do not have assigned private IP addresses. Firewall details: ${jsonencode({ for fw_name, fw in azurerm_firewall.firewall : fw_name => fw.ip_configuration[0].private_ip_address })}"
-  }
 }
