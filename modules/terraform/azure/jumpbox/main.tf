2c4b0635d20705be0d8c84a0d8ebf267a988329a
Xinwei Liu <nadialiuwei@gmail.com>
2026-01-08 10:04:01 +1100

    Update nsg

--- modules/terraform/azure/jumpbox/main.tf
+++ modules/terraform/azure/jumpbox/main.tf
@@ -17,14 +17,14 @@ resource "azurerm_network_security_group" "jumpbox" {
   tags                = merge(var.tags, { "jumpbox" = "true" })
 
   security_rule {
-    name                       = "AllowSSH"
+    name                       = "AllowSSHFromAzureDevOps"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
+    source_address_prefix      = "AzureDevOps"
     destination_address_prefix = "*"
   }
 }
