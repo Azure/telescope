9f4a1a7614473d2407742432f51f9785add00432
Anson Qian <anson627@gmail.com>
2026-01-17 17:08:08 -0500

    refactor MPI operator configuration and add new job templates

--- modules/python/gpu/pkg/mpi.py
+++ modules/python/gpu/pkg/mpi.py
@@ -7,6 +7,7 @@ This module handles the installation of the MPI Operator for distributed workloa
 from utils.logger_config import get_logger, setup_logging
 from utils.retries import execute_with_retries
 from clients.kubernetes_client import KubernetesClient
+from .utils import install_operator
 
 # Configure logging
 setup_logging()
@@ -16,15 +17,23 @@ KUBERNETES_CLIENT = KubernetesClient()
 
 def install_mpi_operator(
     chart_version: str,
+    config_dir: str,
 ) -> None:
     """
     Install MPI Operator using Helm
 
     Args:
-        chart_version: Specific chart version to install
+        chart_version: Specific chart version to install (e.g., "0.5.0")
+        config_dir: Directory containing custom configuration files
     """
-    mpi_file = f"https://raw.githubusercontent.com/kubeflow/mpi-operator/{chart_version}/deploy/v2beta1/mpi-operator.yaml"
-    KUBERNETES_CLIENT.apply_manifest_from_url(mpi_file)
+    install_operator(
+        chart_version=chart_version,
+        operator_name="mpi-operator",
+        config_dir=config_dir,
+        namespace="mpi-operator",
+        repo_name="v3io-stable",
+        repo_url="https://v3io.github.io/helm-charts/stable",
+    )
     execute_with_retries(
         KUBERNETES_CLIENT.wait_for_pods_ready,
         label_selector="app.kubernetes.io/name=mpi-operator",
