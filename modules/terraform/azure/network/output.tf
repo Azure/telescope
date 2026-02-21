a1cd1bb09866c47d71bda5e04233798d0a318af3
Qingqing Zheng <qizhe@microsoft.com>
2026-02-20 17:28:49 -0800

    add private acr

--- modules/terraform/azure/network/output.tf
+++ modules/terraform/azure/network/output.tf
@@ -9,10 +9,7 @@ output "nics" {
 
 output "subnets" {
   description = "Map of subnet names to subnet objects"
-  value = {
-    for subnet_id in azurerm_virtual_network.vnet.subnet[*].id :
-    split("/", subnet_id)[length(split("/", subnet_id)) - 1] => subnet_id
-  }
+  value       = { for name, subnet in azurerm_subnet.subnet : name => subnet.id }
 }
 
 output "vnet_id" {
