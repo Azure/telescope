cd843f3f76a28ce7ef2df09d2063fabd22cc5868
Peter Damianov <ptrdamianov@gmail.com>
2026-02-23 16:39:13 +0000

    telling prometheus to scrape node-exporter

--- modules/python/clusterloader2/utils.py
+++ modules/python/clusterloader2/utils.py
@@ -26,7 +26,7 @@ SCHEDULING_THROUGHPUT_PREFIX = "SchedulingThroughput"
 
 def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provider, cl2_config_file="config.yaml", overrides=False, enable_prometheus=False, tear_down_prometheus=True,
                     enable_exec_service=False, scrape_kubelets=True,
-                    scrape_containerd=True, scrape_ksm=True, scrape_metrics_server=True):
+                    scrape_containerd=True, scrape_ksm=True, scrape_metrics_server=True, scrape_node_exporter=True):
     docker_client = DockerClient()
 
     command = f"""--provider={provider} --v=2
@@ -38,6 +38,7 @@ def run_cl2_command(kubeconfig, cl2_image, cl2_config_dir, cl2_report_dir, provi
 --report-dir /root/perf-tests/clusterloader2/results
 --tear-down-prometheus-server={tear_down_prometheus}
 --prometheus-scrape-kube-state-metrics={scrape_ksm}
+--prometheus-scrape-node-exporter={scrape_node_exporter}
 --prometheus-scrape-metrics-server={scrape_metrics_server}"""
 
     if scrape_containerd:
