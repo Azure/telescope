80d69672aec1a5099d33cc6cdbf4a7df8123417d
Qingqing Zheng <qizhe@microsoft.com>
2026-02-26 17:17:18 -0800

    remove ut since it is more complicated to add dry run mode

--- modules/terraform/azure/aks-cli/output.tf
+++ modules/terraform/azure/aks-cli/output.tf
@@ -7,13 +7,3 @@ output "aks_cli_command" {
   description = "Used for unit tests"
   value       = local.aks_cli_command
 }
-
-output "des_kubelet_object_id" {
-  description = "Used for unit tests"
-  value       = local.aks_kubelet_object_id
-}
-
-output "des_cluster_principal_id" {
-  description = "Used for unit tests"
-  value       = local.aks_system_assigned_principal_id
-}
