088c9c25b3a8fc2d8dd305c608c3e27abcd366f6
wonderyl <wonderyl@gmail.com>
2026-03-24 19:59:58 +1100

    Remove unused v1 files to make the v2 branch clean.

--- modules/python/gpu/pkg/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-GPU module package.
-
-This package contains modules for installing and managing GPU operators,
-network operators, EFA, and MPI operators.
-"""
--- modules/terraform/azure/public-ip/output.tf
+++
@@ -1,9 +0,0 @@
-output "pip_ids" {
-  description = "Map of public IP names to their objects containing id and ip_address"
-  value = {
-    for ip in azurerm_public_ip.pip : ip.name => {
-      id         = ip.id
-      ip_address = ip.ip_address
-    }
-  }
-}
